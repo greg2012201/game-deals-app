1b5ca171ca93026ec881e79807d7757f007139a8
Grzegorz Dubiel <greg2012201@gmail.com>
2021-04-12 19:51:44 +0200

    Add the return to useEffect for the useMenuVisibilityToggle hook

--- src/hooks/UseMenuVisibility.js
+++ src/hooks/UseMenuVisibility.js
@@ -4,11 +4,14 @@ export const useMenuVisibilityToggle = (changingElement, targetElement) => {
   const [visibility, setVisibility] = useState(true)
   useEffect(() => {
     const isTarget = () => {
-      setVisibility(changingElement.current.getBoundingClientRect().bottom <= targetElement.current.getBoundingClientRect().bottom + 5)
+      setVisibility(changingElement.current.getBoundingClientRect().bottom <= targetElement.current.getBoundingClientRect().bottom - 5)
     }
     if (changingElement && targetElement) {
       isTarget()
       window.addEventListener('scroll', isTarget)
+    }
+    return () => {
+      window.removeEventListener('scroll', isTarget)
     }
   }, [changingElement, targetElement])
   return visibility
