d6a627d20f2698ba99d4f0d9a5148adac38dd1f5
Grzegorz Dubiel <greg2012201@gmail.com>
2021-04-12 13:17:15 +0200

    Add visibility logic for the  Menu

--- src/components/molecules/Menu/Menu.js
+++ src/components/molecules/Menu/Menu.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MenuButton from '../../atoms/MenuButton/MenuButton'
 import { Wrapper } from './Menu.style'
 
 export const Menu = ({ receivedRefs }) => {
-  console.log(receivedRefs)
+  const [visibility, setVisibility] = useState(true)
+  const wrapperRef = useRef(null)
+
+  useEffect(() => {
+    const isTarget = () => {
+      setVisibility(receivedRefs.current.getBoundingClientRect().bottom >= wrapperRef.current.getBoundingClientRect().bottom + 5)
+    }
+    if (receivedRefs) {
+      isTarget()
+      window.addEventListener('scroll', isTarget)
+    }
+  }, [receivedRefs])
   return (
-    <Wrapper>
+    <Wrapper ref={wrapperRef} isVisible={visibility}>
       <MenuButton />
     </Wrapper>
   )
--- src/components/molecules/Menu/Menu.style.js
+++ src/components/molecules/Menu/Menu.style.js
@@ -5,10 +5,12 @@ export const Wrapper = styled.div`
   top: 0;
   left: 0;
   right: 0;
-  z-index: 100;
+  z-index: ${({ isVisible }) => (isVisible ? '100' : '0')};
   bottom: calc(100vh - 56px);
   display: flex;
   grid-column: 1/2;
   grid-row: 1/2;
   background-color: ${({ theme }) => theme.colors.grey};
+  opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
+  transition: 0.3s;
 `
