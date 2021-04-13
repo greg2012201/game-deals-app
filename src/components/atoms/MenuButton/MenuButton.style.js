0420cf2cba5bc67abb17c946fa7293277fd08d19
Grzegorz Dubiel <greg2012201@gmail.com>
2021-04-13 09:06:07 +0200

    Add style for MenuButton

--- src/components/atoms/MenuButton/MenuButton.style.js
+++ src/components/atoms/MenuButton/MenuButton.style.js
@@ -5,11 +5,12 @@ export const Wrapper = styled.button`
   align-items: center;
   width: 160px;
   border: none;
-  background-color: ${({ theme }) => theme.colors.semiTransparentBlack};
+  background-color: ${({ theme, toggle }) => (toggle ? theme.colors.white : theme.colors.semiTransparentBlack)};
+  transition: 0.2s;
   h1 {
     font-family: 'Krub';
     font-size: ${({ theme }) => theme.fontSize.m};
-    color: ${({ theme }) => theme.colors.white};
+    color: ${({ theme, toggle }) => (toggle ? theme.colors.black : theme.colors.white)};
     font-weight: bold;
   }
   svg {
