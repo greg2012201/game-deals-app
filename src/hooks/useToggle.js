853efb94cb9e3e82694b92ba621312113140c48c
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-01 23:22:37 +0200

    Remove the useToggle hook

--- src/hooks/useToggle.js
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react'
-
-export const useToggle = (initialValue = false) => {
-  const [value, setValue] = useState(initialValue)
-  return [value, () => setValue((state) => !state)]
-}
