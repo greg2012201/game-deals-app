f4a5f522db6a583648c722c384703a0171b3feb3
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-07 10:28:01 +0200

    Add the test-utils

---
+++ src/test-utils.js
@@ -0,0 +1,16 @@
+// test-utils.js
+import { render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'assets/styles/theme'
+
+const AllTheProviders = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options })
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }
