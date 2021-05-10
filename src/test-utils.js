fdabd8d553c557d5d72e2dbf34524ffd4486850f
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-10 10:15:06 +0200

    Add the MemoryRouter to test-utils.js

--- src/test-utils.js
+++ src/test-utils.js
@@ -2,9 +2,14 @@
 import { render } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'assets/styles/theme'
+import { MemoryRouter } from 'react-router-dom'
 
 const AllTheProviders = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  return (
+    <MemoryRouter>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </MemoryRouter>
+  )
 }
 
 const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options })
