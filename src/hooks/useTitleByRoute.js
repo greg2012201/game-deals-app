a20207f6e9796620f04cd222efcb2193aa93a25f
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-20 16:26:51 +0200

    Create the useTitleByRoute
    Use the useTitleByRoute to find and display a genre name form fetched data. Use it inside the Dashboard component

---
+++ src/hooks/useTitleByRoute.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+
+export const useTitleByRoute = (data, slug) => {
+  const [title, setTitle] = useState('')
+  useEffect(() => {
+    if (!slug) return
+    const filtredData = data.find((e) => (e.slug.toLowerCase() === slug.toLowerCase() ? e.name : null))
+    return filtredData ? setTitle(filtredData.name) : null
+  }, [slug, data])
+  return { title }
+}
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ProductList from 'components/oraganisms/GamesList/GamesList'
 import { useParams } from 'react-router'
 import { Wrapper } from './Dashboard.style'
+import { GamesContext } from 'providers/GamesDataProvider'
+import { useTitleByRoute } from 'hooks/useTitleByRoute'
 const Dashboard = () => {
+  const {
+    data: {
+      genresData: { data },
+    },
+  } = useContext(GamesContext)
   const { page, slug } = useParams()
+  const { title } = useTitleByRoute(data, slug)
   return (
     <Wrapper>
-      <h3>{slug ? slug : page}</h3>
+      <h3>{slug ? title : page}</h3>
       <ProductList />
     </Wrapper>
   )
