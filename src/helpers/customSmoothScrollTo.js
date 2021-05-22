7e589201ddf246f1fdd53e43d2094337fd29cd36
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-22 08:53:05 +0200

    Edit the customSmoothScrollTo function
    Remove the unnecessary option

--- src/helpers/customSmoothScrollTo.js
+++ src/helpers/customSmoothScrollTo.js
@@ -1,11 +1,9 @@
 import smoothscroll from 'smoothscroll-polyfill'
 smoothscroll.polyfill()
-export const customSmoothScrollTo = (option, top, left) => {
-  if (option === 'to') {
-    return window.scrollTo({
-      top: top,
-      left: left,
-      behavior: 'smooth',
-    })
-  }
+export const customSmoothScrollTo = (top, left) => {
+  return window.scrollTo({
+    top: top,
+    left: left,
+    behavior: 'smooth',
+  })
 }
