0e24d0630ed22737c68a816d7b151727359e7a00
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-25 08:30:11 +0200

    Update the GamesDetails' fetching hook
    Rename the useGameDetail to useGameDetails and use the axios all to fetch screenshots and rest of the data

---
+++ src/hooks/useGameDetails.js
@@ -0,0 +1,25 @@
+import { useState, useCallback } from 'react'
+import axios from 'axios'
+
+export const useGameDetails = () => {
+  const [data, setData] = useState({})
+  const [screenshots, setScreenshots] = useState([])
+  const [error, setError] = useState('')
+
+  const fetchData = useCallback(async ([dataUrl, screenshotsUrl]) => {
+    const dataReq = axios.get(dataUrl)
+    const screenshotsReq = axios.get(screenshotsUrl)
+
+    await axios
+      .all([dataReq, screenshotsReq])
+      .then(
+        axios.spread((dataRes, screenshotsRes) => {
+          setData(dataRes.data)
+          setScreenshots(screenshotsRes.data.results)
+        })
+      )
+      .catch(() => setError('Something went wrong'))
+  }, [])
+
+  return { screenshots, data, error, fetchData }
+}
--- src/views/GameDetails.js
+++ src/views/GameDetails.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import { useGameDetail } from 'hooks/useGameDetail'
+import React, { useEffect } from 'react'
+import { useGameDetails } from 'hooks/useGameDetails'
 import { useParams } from 'react-router'
 import { Wrapper } from './GameDetails.style'
+import { RAWGOptions } from 'utils/fetchingOptions'
 
+const { url, key } = RAWGOptions
 const GameDetails = () => {
-  const { slug } = useParams()
-
   const {
-    data: { id, name },
+    data: { name, id, description_raw, background_image_additional },
+    screenshots,
     error,
-  } = useGameDetail(slug)
+    fetchData,
+  } = useGameDetails()
+  const { slug } = useParams()
+  useEffect(() => {
+    fetchData([`${url}/games/${slug}?key=${key}`, `${url}/games/${slug}/screenshots?key=${key}`])
+  }, [fetchData, slug])
 
-  return <Wrapper>{!id && !error ? <p>loading...</p> : !error ? <h1 key={id}>{name}</h1> : <p>{error}</p>}</Wrapper>
+  return (
+    <Wrapper>
+      {!id && !error ? (
+        <p>loading...</p>
+      ) : !error ? (
+        <>
+          <h1 key={id}>{name}</h1>
+          <p>{description_raw}</p>
+          <img data-testid="image" src={background_image_additional} alt={name} />
+          <div>
+            {screenshots.map(({ image, id }) => (
+              <img key={id} data-testid="image" src={image} alt={name} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p>{error}</p>
+      )}
+    </Wrapper>
+  )
 }
 
 export default GameDetails
