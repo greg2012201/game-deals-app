047cf8cc35e9e192d4eca17305c7e6ce1761c7d8
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-27 13:22:51 +0200

    Edit sliders props
    Change the inline styles of the Modal Wrapper and set the pagination's 'clickable' property

--- src/components/molecules/Slider/Slider.js
+++ src/components/molecules/Slider/Slider.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useTheme } from 'styled-components'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation, Pagination, Keyboard } from 'swiper'
 import { Wrapper } from './Slider.style'
 
 const Slider = ({ images, handleSliderClose, isOpen, index }) => {
+  const theme = useTheme()
   SwiperCore.use([Navigation, Pagination, Keyboard])
+
   return (
     <Wrapper
-      style={{ overlay: { zIndex: 10, backgroundColor: 'black' } }}
+      style={{ overlay: { zIndex: 10, backgroundColor: theme.colors.deepBlack } }}
       portalClassName={'ReactModalPortal'}
       className={'MODAL'}
       appElement={document.getElementById('app')}
@@ -16,7 +19,7 @@ const Slider = ({ images, handleSliderClose, isOpen, index }) => {
       isOpen={isOpen}
     >
       {isOpen ? (
-        <Swiper keyboard={true} initialSlide={index} navigation pagination>
+        <Swiper keyboard={true} initialSlide={index} navigation pagination={{ clickable: true }}>
           {images.map(({ image, id }, i) => (
             <SwiperSlide key={i}>
               <img index={i} key={id} data-testid="image" src={image} alt="game" />
