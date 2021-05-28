ada2ebf3b2744c88e5db10ab1c320495f403e2b6
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-28 09:46:49 +0200

    Use the RoundButton component inside the Dashboard
    According to adding RoundButton component remove the old one

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -4,20 +4,23 @@ import { useParams } from 'react-router'
 import { Wrapper } from './Dashboard.style'
 import { GamesContext } from 'providers/GamesDataProvider'
 import { useTitleByRoute } from 'hooks/useTitleByRoute'
-import ReturnButton from 'components/atoms/ReturnButton/ReturnButton'
+import RoundButton from 'components/atoms/RoundButton/RoundButton'
+import { useVisibilityOnScroll } from 'hooks/useVisibilityOnScroll'
+import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo'
 const Dashboard = () => {
   const {
     data: {
       genresData: { data },
     },
   } = useContext(GamesContext)
+  const isVisible = useVisibilityOnScroll()
   const { page, slug } = useParams()
   const { title } = useTitleByRoute(data, slug)
   return (
     <Wrapper>
       <h3>{slug ? title : page}</h3>
       <GamesList />
-      <ReturnButton />
+      <RoundButton onClick={customSmoothScrollTo} isVisible={isVisible} isReturn={true} />
     </Wrapper>
   )
 }
