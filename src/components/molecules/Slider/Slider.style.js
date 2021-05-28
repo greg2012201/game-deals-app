7a68f881456d7fe7dc1bf7b95f29e80f6a647394
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-28 09:51:33 +0200

    Update styles
    Edit styles to the navigation and add custom styles for the Button

--- src/components/molecules/Slider/Slider.style.js
+++ src/components/molecules/Slider/Slider.style.js
@@ -1,6 +1,26 @@
 import styled from 'styled-components'
 import ReactModal from 'react-modal'
+import RoundButton from 'components/atoms/RoundButton/RoundButton'
 export const Wrapper = styled(ReactModal)`
+  .swiper-button {
+    visibility: hidden;
+  }
+  .swiper-button-next,
+  .swiper-button-prev {
+    visibility: hidden;
+    &::after {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    @media (min-width: 768px) {
+      & {
+        visibility: visible;
+      }
+    }
+  }
+
+  .swiper-pagination-bullet-active {
+    background: ${({ theme }) => theme.colors.white};
+  }
   .swiper-container {
     position: absolute;
     display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
@@ -11,16 +31,29 @@ export const Wrapper = styled(ReactModal)`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    opacity: 1;
+
     width: 100%;
     width: 100%;
-    max-height: 90vh;
+    max-height: 100vh;
+
     img {
       width: 100%;
+      max-height: 100vh;
     }
   }
   background-color: transparent;
   &:focus {
     outline: none;
+  }
+`
+export const Button = styled(RoundButton)`
+  bottom: 10%;
+  right: 50%;
+  transform: translateX(50%);
+  @media (min-width: 600px) {
+    & {
+      bottom: 90vh;
+      right: 50px;
+    }
   }
 `
