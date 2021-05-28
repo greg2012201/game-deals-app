81900fe3f29a87d04a638b7c6d8093285ddb98e5
Grzegorz Dubiel <greg2012201@gmail.com>
2021-05-28 09:43:26 +0200

    Create the RoundButton styles

---
+++ src/components/atoms/RoundButton/RoundButton.style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.button`
+  padding: 0;
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  border: none;
+  z-index: 1000;
+  cursor: pointer;
+  transition: 0.2;
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+  }
+`
