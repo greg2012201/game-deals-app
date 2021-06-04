ff50946df4b2c048645913b6598cc88475b87b5e
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-04 10:42:10 +0200

    Update the TextContainer's styles
    Remove a ContentWrapper's padding and StyledP's margin

--- src/components/molecules/TextContainer/TextContainer.style.js
+++ src/components/molecules/TextContainer/TextContainer.style.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const ContentWrapper = styled.div`
   position: relative;
   line-height: 20px;
-  padding: 10px;
   overflow: hidden;
 `
 
@@ -11,6 +10,7 @@ export const StyledP = styled.p`
   color: ${({ theme }) => theme.colors.white};
   font-family: Lato;
   text-align: justify;
+  margin: 0;
 `
 
 export const ViewWrapper = styled.div`
