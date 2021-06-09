090550304f1ed3a3e20cc7b984d58c7516b719e9
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-09 08:43:25 +0200

    useTextContainer update
    Add more elements dimensions to the hook logic for more flexibility. Use the getDimension helper for better source code clarity

--- src/components/molecules/TextContainer/useTextContainer.js
+++ src/components/molecules/TextContainer/useTextContainer.js
@@ -1,11 +1,17 @@
+import { getDimensions } from 'helpers/getDimensions'
 import { useState, useEffect, useCallback } from 'react'
 
 export const useTextContainer = (view, paragraph) => {
   const [isOpen, setOpen] = useState(false)
   const [isButtonVisible, setButtonVisible] = useState(false)
   const [initialWidth, setInitialWidth] = useState(window.innerWidth)
+
   const heightComparison = useCallback(() => {
-    if (paragraph.current.getBoundingClientRect().height < view.current.getBoundingClientRect().height) {
+    const { height: paragraphHeight, margin: paragraphMargin, padding: paragraphPadding, lineHeight: paragraphLineHeight } = getDimensions(
+      paragraph.current
+    )
+    const { height: viewHeight } = getDimensions(view.current)
+    if (paragraphHeight + paragraphMargin - paragraphPadding < viewHeight + paragraphLineHeight || paragraphHeight === 0) {
       setButtonVisible(false)
     } else setButtonVisible(true)
   }, [paragraph, view])
