8e9b6f5d92192b901772c075f1f0b1b687ee2592
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-09 08:36:46 +0200

    TextContainer.style update
    add margin top for the button inside ContentWrapper and StyledP. Use the viewHeight prop in ViewWrapper styles

--- src/components/molecules/TextContainer/TextContainer.style.js
+++ src/components/molecules/TextContainer/TextContainer.style.js
@@ -4,16 +4,19 @@ export const ContentWrapper = styled.div`
   position: relative;
   line-height: 20px;
   overflow: hidden;
+  button {
+    margin-top: 20px;
+  }
 `
 
 export const StyledP = styled.p`
   color: ${({ theme }) => theme.colors.white};
   font-family: Lato;
   text-align: justify;
-  margin: 0;
+  margin-top: 20px;
 `
 
 export const ViewWrapper = styled.div`
   overflow: hidden;
-  max-height: ${({ isOpen, viewHeight }) => (isOpen ? 'auto' : '200px')};
+  max-height: ${({ isOpen, viewHeight }) => (isOpen ? 'auto' : `${viewHeight}px`)};
 `
