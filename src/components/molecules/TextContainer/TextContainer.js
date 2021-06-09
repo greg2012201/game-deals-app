21ae594b63088307338c6d7a67ba2bb9f8dabe3c
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-09 08:34:16 +0200

    Update the TextContainer
    Change the declaration of the TextContainer function from the old function statement to an arrow function statement. Add the viewHight prop

--- src/components/molecules/TextContainer/TextContainer.js
+++ src/components/molecules/TextContainer/TextContainer.js
@@ -2,14 +2,14 @@ import React, { useRef } from 'react'
 import { useTextContainer } from './useTextContainer'
 import { ContentWrapper, ViewWrapper, StyledP } from './TextContainer.style'
 import { Button } from 'components/atoms/Button/Button'
-function TextContainer({ children }) {
+const TextContainer = ({ children, viewHeight = 200 }) => {
   const view = useRef(null)
   const paragraph = useRef(null)
 
   const { isOpen, isButtonVisible, setOpen } = useTextContainer(view, paragraph)
   return (
     <ContentWrapper>
-      <ViewWrapper ref={view} isOpen={isOpen}>
+      <ViewWrapper viewHeight={viewHeight} ref={view} isOpen={isOpen}>
         <StyledP ref={paragraph}>{children}</StyledP>
       </ViewWrapper>
       <Button isLight isVisible={isButtonVisible} onClick={() => setOpen((toggle) => !toggle)}>
