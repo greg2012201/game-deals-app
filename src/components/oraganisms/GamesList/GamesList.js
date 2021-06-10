321b24ad369ad4d2d10f230627fc3acdcd5158ec
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-10 10:51:32 +0200

    Update the GamesList
    Move fetching and routing logic from GamesList to the Dashboard. Set the scrollThreshold prop for the InfiniteScroll

--- src/components/oraganisms/GamesList/GamesList.js
+++ src/components/oraganisms/GamesList/GamesList.js
@@ -1,42 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ProductCard from 'components/molecules/GamesListItem/GamesListItem'
 import { StyledEndMessage, StyledList, StyledLoader } from './GamesList.style'
-import { useParams } from 'react-router'
-import { useFetchData } from 'hooks/useFetchData'
-import { RAWGOptions } from 'utils/fetchingOptions'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useTheme } from 'styled-components'
 import ErrorMessage from 'components/molecules/ErrorMessage/ErrorMessage'
 import SkeletonLoader from 'components/atoms/SkeletonLoader/SkeletonLoader'
 
-const { url, key } = RAWGOptions
-const GamesList = () => {
+const GamesList = ({ fetchMoreData, fetchedData: { data, loading, error, nextPage, limit } }) => {
   const theme = useTheme()
-  const {
-    fetchedData: { data, nextPage, limit, error, loading },
-    resetData,
-    fetchData,
-    getCancelToken,
-  } = useFetchData()
-  const { page, slug } = useParams()
-  useEffect(() => {
-    const cancelToken = getCancelToken()
-    if (page === 'Home') {
-      fetchData(`${url}/games?key=${key}`, cancelToken)
-    } else if (page === 'genres') fetchData(`${url}/games?genres=${slug}&key=${key}`, cancelToken)
-    return () => {
-      resetData(cancelToken)
-    }
-  }, [page, fetchData, slug, resetData, getCancelToken])
-
   const handleFetchMoreData = () => {
     if (!nextPage) return
-
-    fetchData(nextPage)
+    return fetchMoreData(nextPage)
   }
-
   return (
     <InfiniteScroll
+      scrollThreshold={'200px'}
       dataLength={data.length}
       next={handleFetchMoreData}
       hasMore={data.length <= limit && nextPage !== null}
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import GamesList from 'components/oraganisms/GamesList/GamesList'
 import { useParams } from 'react-router'
 import { Wrapper } from './Dashboard.style'
@@ -8,6 +8,9 @@ import RoundButton from 'components/atoms/RoundButton/RoundButton'
 import { useVisibilityOnScroll } from 'hooks/useVisibilityOnScroll'
 import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo'
 import Title from 'components/atoms/Title/Title'
+import { useFetchData } from 'hooks/useFetchData'
+import { RAWGOptions } from 'utils/fetchingOptions'
+const { url, key } = RAWGOptions
 const Dashboard = () => {
   const {
     data: {
@@ -17,10 +20,23 @@ const Dashboard = () => {
   const isVisible = useVisibilityOnScroll()
   const { page, slug } = useParams()
   const { title } = useTitleByRoute(data, slug)
+
+  const { fetchedData, resetData, fetchData, getCancelToken } = useFetchData()
+
+  useEffect(() => {
+    const cancelToken = getCancelToken()
+    if (page === 'Home') {
+      fetchData(`${url}/games?key=${key}`, cancelToken)
+    } else if (page === 'genres') fetchData(`${url}/games?genres=${slug}&key=${key}`, cancelToken)
+    return () => {
+      resetData(cancelToken)
+    }
+  }, [page, fetchData, slug, resetData, getCancelToken])
+
   return (
     <Wrapper>
       <Title>{slug ? title : page}</Title>
-      <GamesList />
+      <GamesList fetchMoreData={fetchData} fetchedData={fetchedData} />
       <RoundButton onClick={customSmoothScrollTo} isVisible={isVisible} isReturn={true} />
     </Wrapper>
   )
