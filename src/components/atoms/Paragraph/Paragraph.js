85d2f91b0beb0cd9c3f0cb89a93fefb601b888a2
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-15 23:17:50 +0200

    Remove a text-align and margin-top properties

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -3,6 +3,4 @@ import styled from 'styled-components'
 export const Paragraph = styled.p`
   color: ${({ theme }) => theme.colors.white};
   font-family: Lato;
-  text-align: justify;
-  margin-top: 20px;
 `
