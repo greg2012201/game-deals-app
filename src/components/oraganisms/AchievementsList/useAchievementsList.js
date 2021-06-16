1986d5c05d0b22f5135828438e20fee202701872
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-16 09:44:18 +0200

    Install the rc-pagination

---
+++ src/components/oraganisms/AchievementsList/useAchievementsList.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import { useState, useCallback } from 'react'
+
+export const useAchievementsList = () => {
+  const [achievements, setAchievements] = useState([])
+  const [page, setPage] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = useCallback(async (url, source = null) => {
+    setLoading(true)
+    try {
+      const {
+        data: { next, previous, count, results },
+      } = await axios.get(
+        url,
+        source
+          ? {
+              cancelToken: source.token,
+            }
+          : null
+      )
+      setAchievements(results)
+      setPage({ next, previous, count })
+      setLoading(false)
+    } catch (e) {
+      return setLoading(false)
+    }
+  }, [])
+  const getCancelToken = useCallback(() => {
+    return axios.CancelToken.source()
+  }, [])
+  const resetData = useCallback((source) => {
+    source.cancel()
+  }, [])
+
+  return { fetchData, achievements, getCancelToken, resetData, page, loading }
+}
