19a45372b2eb4591ca3a99134d69dd8aceaecd05
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-17 10:27:21 +0200

    Add new styles for the Achievements component mainly for pagination

--- src/components/oraganisms/AchievementsList/Achievements.style.js
+++ src/components/oraganisms/AchievementsList/Achievements.style.js
@@ -6,18 +6,55 @@ export const StyledAchivementsList = styled.ul`
   max-width: 340px;
   grid-gap: 20px;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(100px, 130px));
-  grid-template-rows: repeat(auto-fill, minmax(100px, 150px));
-
+  grid-template-columns: repeat(50%);
+  grid-template-rows: 30px 170px 150px;
+  min-height: 400px;
+  h2 {
+    grid-column: 1/3;
+    grid-row: 1;
+  }
   .pagination {
     grid-column: 1/3;
+    grid-row: 4;
     margin: 0;
     padding: 0;
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
     list-style: none;
+    color: ${({ theme }) => theme.colors.lighterGrey};
+    font-family: Lato;
+    font-size: ${({ theme }) => theme.fontSize.s};
+  }
+  .rc-pagination-prev,
+  .rc-pagination-next {
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.violet};
+    font-size: ${({ theme }) => theme.fontSize.xl};
+    transition: 0.3s;
+    cursor: pointer;
+  }
+  .rc-pagination-disabled {
+    color: ${({ theme }) => theme.colors.grey};
+  }
+  .rc-pagination-item {
+    transition: 0.3s;
   }
   .rc-pagination-item-active {
-    background-color: ${({ theme }) => theme.colors.violet};
+    background-color: ${({ theme }) => theme.colors.transparentViolet};
+    border-radius: 50%;
+
+    color: ${({ theme }) => theme.colors.white};
+  }
+  .rc-pagination-jump-next,
+  .rc-pagination-jump-prev,
+  .rc-pagination-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
   }
 `
