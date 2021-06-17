fb6c8b1c03c4f8d66f767fb7ee4d3eb6ddac0121
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-17 10:00:53 +0200

    Update the usePagination hook
    Cancel fetch when a component is unmount. Add the pageSize parameter and include it in the fethc

--- src/components/oraganisms/AchievementsList/usePagination.js
+++ src/components/oraganisms/AchievementsList/usePagination.js
@@ -2,24 +2,30 @@ import { useEffect, useState } from 'react'
 import { RAWGOptions } from 'utils/fetchingOptions'
 const { url, key } = RAWGOptions
 
-export const usePagination = ({ achievementsFor, fetchData, resetData, getCancelToken, listRef }) => {
+export const usePagination = ({ pageSize, achievementsFor, fetchData, resetData, getCancelToken, listRef }) => {
   const [currentPage, setCurrentPage] = useState(1)
   const [initialPage, setInitialPage] = useState(true)
   const handleOnPageChange = (current) => {
     if (listRef.current === null || undefined) return
-    listRef.current.scrollIntoView()
     return current !== currentPage ? setCurrentPage(current) : null
   }
   useEffect(() => {
     setInitialPage(false)
     setCurrentPage(1)
+    return () => {
+      setCurrentPage(1)
+      setInitialPage(true)
+    }
   }, [achievementsFor])
   useEffect(() => {
     const cancelToken = getCancelToken()
-    if (initialPage) return
-    fetchData(`${url}/games/${achievementsFor}/achievements?page=${currentPage}&key=${key}`, cancelToken)
+    if (initialPage) {
+      setCurrentPage(1)
+      return resetData(cancelToken)
+    }
+    fetchData(`${url}/games/${achievementsFor}/achievements?page=${currentPage}&page_size=${pageSize}&key=${key}`, cancelToken)
     return () => (!initialPage ? resetData(cancelToken) : null)
-  }, [achievementsFor, currentPage, fetchData, initialPage, getCancelToken, resetData])
+  }, [achievementsFor, currentPage, fetchData, initialPage, getCancelToken, resetData, pageSize])
 
   return { handleOnPageChange, currentPage }
 }
