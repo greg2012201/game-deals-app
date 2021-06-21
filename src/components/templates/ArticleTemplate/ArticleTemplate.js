52c22b0aae39c5a6d80f116f2184d0910837352c
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-21 13:16:55 +0200

    Update the ArticleTemplate
    Add conditional rendering by window width for the TextContainer

--- src/components/templates/ArticleTemplate/ArticleTemplate.js
+++ src/components/templates/ArticleTemplate/ArticleTemplate.js
@@ -2,12 +2,15 @@ import Title from 'components/atoms/Title/Title'
 import TextContainer from 'components/molecules/TextContainer/TextContainer'
 import React from 'react'
 import { ArticleWrapper } from './ArticleTemplate.style'
+import { Paragraph } from 'components/atoms/Paragraph/Paragraph'
+import { useWindowSize } from 'react-use'
 
 const ArticleTemplate = ({ textContent, title }) => {
+  const { width: windowWidth } = useWindowSize()
   return (
     <ArticleWrapper>
       <Title titleType="h2">{title}</Title>
-      <TextContainer>{textContent}</TextContainer>
+      {windowWidth < 980 ? <TextContainer>{textContent}</TextContainer> : <Paragraph>{textContent}</Paragraph>}
     </ArticleWrapper>
   )
 }
