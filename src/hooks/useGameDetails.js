ebf9c3bde1eb6927f5badfa7004cd1b285494871
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-23 10:25:14 +0200

    Include the loading in the useGameDetails' state

--- src/hooks/useGameDetails.js
+++ src/hooks/useGameDetails.js
@@ -5,20 +5,27 @@ export const useGameDetails = () => {
   const [data, setData] = useState({})
   const [screenshots, setScreenshots] = useState([])
   const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true )
+ 
 
   const fetchData = useCallback(async ([dataUrl, screenshotsUrl]) => {
     const dataReq = axios.get(dataUrl)
     const screenshotsReq = axios.get(screenshotsUrl)
-
+setLoading(true)
     await axios
       .all([dataReq, screenshotsReq])
       .then(
+       
         axios.spread((dataRes, screenshotsRes) => {
+          setLoading(false)
           setData(dataRes.data)
           setScreenshots(screenshotsRes.data.results)
         })
       )
-      .catch(() => setError('Something went wrong'))
+      .catch(() => { 
+        setLoading(false)
+        return setError('Something went wrong')})
   }, [])
-  return { screenshots, data, error, fetchData }
+
+  return { screenshots, data, error, fetchData, loading }
 }
