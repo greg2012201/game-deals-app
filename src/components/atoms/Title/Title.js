e941b4bb2b55949e4b009f20fed305eb00c2af9a
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-26 11:47:12 +0200

    Create the TitleSkeletonLoader
    Include the TitleSkeletonLoader. Replace the old function declaration  syntax  with an arrow function syntax

--- src/components/atoms/Title/Title.js
+++ src/components/atoms/Title/Title.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { StyledH1, StyledH2, StyledH3, StyledH4 } from './Title.style'
+import TitleSkeletonLoader from './TitleSkeletonLoader'
 const titleTypes = { h1: 'h1', h2: 'h2', h3: 'h3', h4: 'h4' }
-function Title({ children, titleType }) {
-  switch (titleType) {
-    case titleTypes.h1:
-      return <StyledH1>{children}</StyledH1>
-    case titleTypes.h2:
-      return <StyledH2>{children} </StyledH2>
-    case titleTypes.h3:
-      return <StyledH3>{children}</StyledH3>
-    case titleTypes.h4:
-      return <StyledH4>{children}</StyledH4>
-    default:
-      return <StyledH1>{children}</StyledH1>
+const Title = ({ children, titleType, isLoading }) => {
+  if (isLoading) {
+    return (
+      <StyledH1>
+        <TitleSkeletonLoader />
+      </StyledH1>
+    )
+  } else {
+    switch (titleType) {
+      case titleTypes.h1:
+        return <StyledH1>{children}</StyledH1>
+      case titleTypes.h2:
+        return <StyledH2>{children} </StyledH2>
+      case titleTypes.h3:
+        return <StyledH3>{children}</StyledH3>
+      case titleTypes.h4:
+        return <StyledH4>{children}</StyledH4>
+      default:
+        return <StyledH1>{children}</StyledH1>
+    }
   }
 }
 
