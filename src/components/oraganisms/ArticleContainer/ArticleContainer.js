83f5c913eb71e6a367b29daa03112825d61b1de6
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-28 16:52:16 +0200

    Create the ArticleContainerSkeletonLoader.
    Include a skeleton in the ArticleContainer component

--- src/components/oraganisms/ArticleContainer/ArticleContainer.js
+++ src/components/oraganisms/ArticleContainer/ArticleContainer.js
@@ -3,16 +3,22 @@ import TextContainer from 'components/molecules/TextContainer/TextContainer'
 import React from 'react'
 import { ArticleWrapper } from './ArticleContainer.style'
 import { Paragraph } from 'components/atoms/Paragraph/Paragraph'
+import ArticleContainerSkeletonLoader from './ArticleContainerSkeletonLoader'
 import { useWindowSize } from 'react-use'
 
-const ArticleContainer = ({ children, title }) => {
+const ArticleContainer = ({ children, title, isLoading, data }) => {
   const { width: windowWidth } = useWindowSize()
-  return (
-    <ArticleWrapper>
-      <Title titleType="h2">{title}</Title>
-      {windowWidth < 980 ? <TextContainer>{children}</TextContainer> : <Paragraph>{children}</Paragraph>}
-    </ArticleWrapper>
-  )
+
+  if (isLoading || Object.keys(data).length === 0) {
+    return <ArticleContainerSkeletonLoader />
+  } else {
+    return (
+      <ArticleWrapper>
+        <Title titleType="h2">{title}</Title>
+        {windowWidth < 980 ? <TextContainer>{children}</TextContainer> : <Paragraph>{children}</Paragraph>}
+      </ArticleWrapper>
+    )
+  }
 }
 
 export default ArticleContainer
