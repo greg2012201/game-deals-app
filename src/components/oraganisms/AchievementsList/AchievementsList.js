decf858031014f1ed0eeaf82ee96e99ed1fa34aa
Grzegorz Dubiel <greg2012201@gmail.com>
2021-06-29 12:08:22 +0200

    Include a skeleton loader in the AchievementsList component

--- src/components/oraganisms/AchievementsList/AchievementsList.js
+++ src/components/oraganisms/AchievementsList/AchievementsList.js
@@ -5,24 +5,31 @@ import { StyledAchivementsList } from './Achievements.style'
 import { useAchievementsListData } from './useAchievementsListData'
 import { usePagination } from './usePagination'
 import Title from 'components/atoms/Title/Title'
+import AchievementsListItemSkeletonLoader from 'components/molecules/AchievementsListItem/AchievementsListItemSkeletonLoader'
 const pageSize = 4
 const AchievementsList = ({ achievementsFor }) => {
   const listRef = useRef(null)
 
   const {
     fetchData,
+    loading,
     getCancelToken,
     resetData,
     achievements,
     page: { count },
   } = useAchievementsListData()
   const { handleOnPageChange, currentPage } = usePagination({ pageSize, achievementsFor, fetchData, resetData, getCancelToken, listRef })
+
   return (
     <StyledAchivementsList ref={listRef}>
       <Title titleType="h2">Achievements</Title>
-      {achievements.map((achievements, i) => {
-        return <AchievementsListItem key={i} achievementsData={achievements} />
-      })}
+      {loading || achievements.length === 0
+        ? Array(4)
+            .fill('')
+            .map((e, i) => <AchievementsListItemSkeletonLoader key={i} />)
+        : achievements.map((achievements, i) => {
+            return <AchievementsListItem key={i} achievementsData={achievements} />
+          })}
       <Pagination
         onChange={handleOnPageChange}
         pageSize={pageSize}
