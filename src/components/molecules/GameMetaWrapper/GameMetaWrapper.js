23adc533f768e6d604bfd5045a0230cc1e340520
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-01 13:48:41 +0200

    GameMetaWrapper update
    Replace the old data logic with the useConvertNestedObjectsArrayIntoAnArrayOfObject custom hook. Add a data condition to the jsx structure.

--- src/components/molecules/GameMetaWrapper/GameMetaWrapper.js
+++ src/components/molecules/GameMetaWrapper/GameMetaWrapper.js
@@ -1,45 +1,44 @@
 import { Score } from 'components/atoms/Score/Score'
 import Title from 'components/atoms/Title/Title'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import GameMetaItem from '../GamesMetaItem/GameMetaItem'
 import { MetaContentWrapper } from './GameMetaWrapper.style'
+import { useConvertNestedObjectsInArrayIntoAnArrayOfObjects } from 'hooks/useConvertNestedObjectsInArrayIntoAnArrayOfObjects'
 
 const GameMetaWrapper = ({
+  data,
   data: { parent_platforms: parentPlatforms, released, publishers, website, genres, metacritic, esrb_rating: ageRating, developers },
 }) => {
-  let platformData = []
-  parentPlatforms.map(({ platform }) => platformData.push(platform))
+  const { transformedData: platformData } = useConvertNestedObjectsInArrayIntoAnArrayOfObjects(parentPlatforms)
 
-  return (
-    <>
-      <MetaContentWrapper>
-        <Title titleType="h2">Meta</Title>
-        <GameMetaItem title="Platforms" data={platformData} />
-        <GameMetaItem title="Genres">
-          {genres.map(({ id, name, slug }) => (
-            <Link to={`/genres/${slug}`} key={id}>
-              {name}
-            </Link>
-          ))}
+  return Object.keys(data).length !== 0 ? (
+    <MetaContentWrapper>
+      <Title titleType="h2">Meta</Title>
+      <GameMetaItem title="Platforms" data={platformData} />
+      <GameMetaItem title="Genres">
+        {genres.map(({ id, name, slug }) => (
+          <Link to={`/genres/${slug}`} key={id}>
+            {name}
+          </Link>
+        ))}
+      </GameMetaItem>
+      <GameMetaItem title="Release Date" text={released} />
+      <GameMetaItem title="Publishers" data={publishers} />
+      <GameMetaItem title="Developer" data={developers} />
+      {metacritic ? (
+        <GameMetaItem title="Metascore">
+          <Score value={metacritic}>{metacritic}</Score>
         </GameMetaItem>
-        <GameMetaItem title="Release Date" text={released} />
-        <GameMetaItem title="Publishers" data={publishers} />
-        <GameMetaItem title="Developer" data={developers} />
-        {metacritic ? (
-          <GameMetaItem title="Metascore">
-            <Score value={metacritic}>{metacritic}</Score>
-          </GameMetaItem>
-        ) : null}
-        {ageRating ? <GameMetaItem title="Age rating" text={ageRating.name} /> : null}
-        <GameMetaItem handleClassName={'hasLink'} title="Website">
-          <a target={'_blank'} rel={'noreferrer'} href={website}>
-            {website}
-          </a>
-        </GameMetaItem>
-      </MetaContentWrapper>
-    </>
-  )
+      ) : null}
+      {ageRating ? <GameMetaItem title="Age rating" text={ageRating.name} /> : null}
+      <GameMetaItem handleClassName={'hasLink'} title="Website">
+        <a target={'_blank'} rel={'noreferrer'} href={website}>
+          {website}
+        </a>
+      </GameMetaItem>
+    </MetaContentWrapper>
+  ) : null
 }
 
 export default GameMetaWrapper
