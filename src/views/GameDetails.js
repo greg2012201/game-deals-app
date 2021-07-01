1c5a297150549ac12e2b695c18376793f3f257a8
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-01 14:57:11 +0200

    Update the GameDetails component
    Remove the GamesList data fetching logic. Rename details data variables. Remove the details data condition form the jsx. Set the new props for the GamesList component

--- src/views/GameDetails.js
+++ src/views/GameDetails.js
@@ -8,9 +8,8 @@ import Title from 'components/atoms/Title/Title'
 import ArticleContainer from 'components/oraganisms/ArticleContainer/ArticleContainer'
 import GameMetaWrapper from 'components/molecules/GameMetaWrapper/GameMetaWrapper'
 import PCRequirements from 'components/molecules/PCRequirements/PCRequirements'
-import { InformationsTemplate } from 'components/templates/InformationsTemplate/InformationsTemplate'
+import InformationsTemplate from 'components/templates/InformationsTemplate/InformationsTemplate'
 import GamesList from 'components/oraganisms/GamesList/GamesList'
-import { useGamesList } from 'hooks/useGamesList'
 import RoundButton from 'components/atoms/RoundButton/RoundButton'
 import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo'
 import AchievementsList from 'components/oraganisms/AchievementsList/AchievementsList'
@@ -20,25 +19,17 @@ import Screenshots from 'components/oraganisms/Screenshots/Screenshots'
 const { url, key } = RAWGOptions
 const GameDetails = () => {
   const { slug } = useParams()
-  const { fetchedData: fetchedGameListData, fetchData: fetchGamesListData, getCancelToken, resetData } = useGamesList()
   const {
     data: detailsData,
     data: { name, id, description_raw: descripton, background_image: backgroundImage, achievements_count: achievementsCount },
     error,
     loading,
-
-    fetchData: fetchDetailsData,
+    fetchData,
   } = useGameDetails()
   useEffect(() => {
     window.scrollTo(0, 0)
-    const cancelToken = getCancelToken()
-    fetchDetailsData(`${url}/games/${slug}?key=${key}`)
-    fetchGamesListData(`${url}/games/${slug}/game-series?key=${key}`)
-
-    return () => {
-      resetData(cancelToken)
-    }
-  }, [fetchDetailsData, fetchGamesListData, getCancelToken, resetData, slug])
+    fetchData(`${url}/games/${slug}?key=${key}`)
+  }, [fetchData, slug])
 
   return (
     <Background>
@@ -51,27 +42,19 @@ const GameDetails = () => {
               {name}
             </Title>
             <RoundButton onClick={customSmoothScrollTo} isReturn={true} />
-
             <Screenshots isLoading={loading} slug={slug} />
             <ArticleContainer data={detailsData} isLoading={loading} title={'About'}>
               {descripton}
             </ArticleContainer>
-            {Object.keys(detailsData).length !== 0 ? (
-              <>
-                <InformationsTemplate>
-                  {achievementsCount > 0 ? <AchievementsList achievementsFor={slug} /> : null}
-                  <GameMetaWrapper data={detailsData} />
-                  <PCRequirements data={detailsData} />
-                </InformationsTemplate>
-              </>
-            ) : null}
-
-            {fetchedGameListData.data.length !== 0 ? (
-              <ListWrapper>
-                <Title titleType="h2">Games like</Title>
-                <GamesList endMessage={false} fetchMoreData={fetchGamesListData} fetchedData={fetchedGameListData} />
-              </ListWrapper>
-            ) : null}
+            <InformationsTemplate isLoading={loading}>
+              {achievementsCount > 0 ? <AchievementsList achievementsFor={slug} /> : null}
+              <GameMetaWrapper data={detailsData} />
+              <PCRequirements data={detailsData} />
+            </InformationsTemplate>
+            <ListWrapper>
+              <Title titleType="h2">Games like</Title>
+              <GamesList endMessage={false} fecthingRoute={`/games/${slug}/game-series?`} />
+            </ListWrapper>
           </Mask>
         </Wrapper>
       )}
