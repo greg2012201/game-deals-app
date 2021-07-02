c9d5f60f1ffc38d3ad0d0ca6b8f6151d5fdf16c7
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-02 08:59:41 +0200

    GamesList update
    Add a title option. Add the StyledListWrapper to the jsx structure

--- src/components/oraganisms/GamesList/GamesList.js
+++ src/components/oraganisms/GamesList/GamesList.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react'
 import ProductCard from 'components/molecules/GamesListItem/GamesListItem'
-import { StyledEndMessage, StyledList, StyledLoader } from './GamesList.style'
+import { StyledEndMessage, StyledListWrapper, StyledList, StyledLoader } from './GamesList.style'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useTheme } from 'styled-components'
 import ErrorMessage from 'components/molecules/ErrorMessage/ErrorMessage'
 import GamesListSkeletonLoader from 'components/atoms/GamesListSkeletonLoader/GamesListSkeletonLoader'
 import { useGamesList } from 'hooks/useGamesList'
 import { RAWGOptions } from 'utils/fetchingOptions'
+import Title from 'components/atoms/Title/Title'
 const { url, key } = RAWGOptions
-const GamesList = ({ endMessage = 'Yay! You have seen it all', fecthingRoute }) => {
+const GamesList = ({ endMessage = 'Yay! You have seen it all', title = null, fecthingRoute }) => {
   const theme = useTheme()
 
   const {
@@ -31,30 +32,33 @@ const GamesList = ({ endMessage = 'Yay! You have seen it all', fecthingRoute }) 
     return fetchData(nextPage)
   }
   return (
-    <InfiniteScroll
-      scrollThreshold={'200px'}
-      dataLength={data.length}
-      next={handleFetchMoreData}
-      hasMore={data.length <= limit && nextPage !== null}
-      endMessage={endMessage ? <StyledEndMessage style={{ textAlign: 'center' }}>{endMessage}</StyledEndMessage> : null}
-      loader={!error && data.length > 0 ? <StyledLoader type="ThreeDots" color={`${theme.colors.white}`} /> : null}
-    >
-      <StyledList>
-        {loading ? (
-          error && data.length === 0 ? (
-            <ErrorMessage>{error}</ErrorMessage>
+    <StyledListWrapper>
+      <InfiniteScroll
+        scrollThreshold={'200px'}
+        dataLength={data.length}
+        next={handleFetchMoreData}
+        hasMore={data.length <= limit && nextPage !== null}
+        endMessage={endMessage ? <StyledEndMessage style={{ textAlign: 'center' }}>{endMessage}</StyledEndMessage> : null}
+        loader={!error && data.length > 0 ? <StyledLoader type="ThreeDots" color={`${theme.colors.white}`} /> : null}
+      >
+        {title ? <Title titleType="h2">{title}</Title> : null}
+        <StyledList>
+          {loading ? (
+            error && data.length === 0 ? (
+              <ErrorMessage>{error}</ErrorMessage>
+            ) : (
+              Array(20)
+                .fill('')
+                .map((e, i) => <GamesListSkeletonLoader key={i} />)
+            )
           ) : (
-            Array(20)
-              .fill('')
-              .map((e, i) => <GamesListSkeletonLoader key={i} />)
-          )
-        ) : (
-          data.map((data) => {
-            return <ProductCard key={data.id} gamesData={data} />
-          })
-        )}
-      </StyledList>
-    </InfiniteScroll>
+            data.map((data) => {
+              return <ProductCard key={data.id} gamesData={data} />
+            })
+          )}
+        </StyledList>
+      </InfiniteScroll>
+    </StyledListWrapper>
   )
 }
 
