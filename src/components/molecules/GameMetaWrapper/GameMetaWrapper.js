69b9e03d967bd35a7444e636de4748963661dfda
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-03 09:37:00 +0200

    Remove unused imports

--- src/components/molecules/GameMetaWrapper/GameMetaWrapper.js
+++ src/components/molecules/GameMetaWrapper/GameMetaWrapper.js
@@ -1,6 +1,6 @@
 import { Score } from 'components/atoms/Score/Score'
 import Title from 'components/atoms/Title/Title'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import GameMetaItem from '../GamesMetaItem/GameMetaItem'
 import { MetaContentWrapper } from './GameMetaWrapper.style'
