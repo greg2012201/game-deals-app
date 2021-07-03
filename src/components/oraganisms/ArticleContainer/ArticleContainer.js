8a96cb0e7873abdb962c7ab14e6d063765b860f4
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-03 15:53:33 +0200

    Add error management with the ErrorMessage component.

--- src/components/oraganisms/ArticleContainer/ArticleContainer.js
+++ src/components/oraganisms/ArticleContainer/ArticleContainer.js
@@ -4,11 +4,14 @@ import React from 'react'
 import { ArticleWrapper } from './ArticleContainer.style'
 import { Paragraph } from 'components/atoms/Paragraph/Paragraph'
 import ArticleContainerSkeletonLoader from './ArticleContainerSkeletonLoader'
+import ErrorMessage from 'components/atoms/ErrorMessage/ErrorMessage'
 import { useWindowSize } from 'react-use'
 
-const ArticleContainer = ({ children, title, isLoading, data }) => {
+const ArticleContainer = ({ children, title, isLoading, data, error }) => {
   const { width: windowWidth } = useWindowSize()
-
+  if (error) {
+    return <ErrorMessage>Something went wrong</ErrorMessage>
+  }
   if (isLoading || Object.keys(data).length === 0) {
     return <ArticleContainerSkeletonLoader />
   } else {
