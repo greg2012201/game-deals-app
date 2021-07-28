7095773c1dbf89e9d593d0d238a73c6fafa8112a
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-28 10:08:02 +0200

    Add a data condition to the conditional statement

--- src/hooks/useTitleByRoute.js
+++ src/hooks/useTitleByRoute.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 export const useTitleByRoute = (data, slug) => {
   const [title, setTitle] = useState('')
   useEffect(() => {
-    if (!slug) return
+    if (!data || !slug) return
     const filtredData = data.find((e) => (e.slug.toLowerCase() === slug.toLowerCase() ? e.name : null))
     return filtredData ? setTitle(filtredData.name) : null
   }, [slug, data])
