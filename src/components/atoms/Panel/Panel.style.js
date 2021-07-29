dbe06b415af4a8703ba5965bb2f664bcff2b0a54
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-29 18:40:04 +0200

    Update styles

--- src/components/atoms/Panel/Panel.style.js
+++ src/components/atoms/Panel/Panel.style.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+
+  align-items: center;
   position: fixed;
   top: 0;
   left: 0;
@@ -11,8 +12,6 @@ export const Wrapper = styled.div`
   z-index: ${({ isVisible }) => (isVisible ? '1' : '0')};
   bottom: calc(100vh - 56px);
   display: flex;
-  grid-column: 1/2;
-  grid-row: 1/2;
   background-color: ${({ theme }) => theme.colors.grey};
   opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
   transition: 0.3s;
