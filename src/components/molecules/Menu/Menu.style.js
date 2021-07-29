e3237ea80d45a161c23859adc02f11a351bc30ce
Grzegorz Dubiel <greg2012201@gmail.com>
2021-07-29 10:13:20 +0200

    MEnu update
    Remove the Wrapper and all the logic associated with it.

--- src/components/molecules/Menu/Menu.style.js
+++ src/components/molecules/Menu/Menu.style.js
@@ -1,21 +1,6 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 56px;
-  z-index: ${({ isVisible }) => (isVisible ? '1' : '0')};
-  bottom: calc(100vh - 56px);
-  display: flex;
-  grid-column: 1/2;
-  grid-row: 1/2;
-  background-color: ${({ theme }) => theme.colors.grey};
-  opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
-  transition: 0.3s;
-`
 export const DropdownPanel = styled.div`
   display: flex;
   visibility: ${({ isOpen }) => (isOpen ? 'visible' : 'hidden')};
