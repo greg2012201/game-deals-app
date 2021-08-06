0fc637132d701cbdb13a69d5c387b36d4468d36a
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-06 08:56:05 +0200

    Replace the mock data with  fetched data.

--- src/components/molecules/DealsSelect/DealsSelect.js
+++ src/components/molecules/DealsSelect/DealsSelect.js
@@ -1,18 +1,9 @@
 import React from 'react'
+import { useGetListCoveredRegionsQuery } from 'store'
 import Select from '../Select/Select'
 import { Wrapper } from './DealsSelect.style'
-const categories = [
-  { name: 'price', options: ['chocolate', 'strawberry', 'vanilla'] },
-  { name: 'region', options: ['EU', 'US'] },
-]
 const DealsSelect = () => {
-  return (
-    <Wrapper>
-      {categories.map(({ options, name }) => (
-        <Select key={name} options={options} title={name} />
-      ))}
-    </Wrapper>
-  )
+  const { data, isLoading } = useGetListCoveredRegionsQuery()
+  return <Wrapper>{!isLoading && data.map(({ name, options }) => <Select key={name} options={options} title={name} />)}</Wrapper>
 }
-
 export default DealsSelect
