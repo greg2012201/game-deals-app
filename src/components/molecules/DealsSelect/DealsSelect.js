33b9a5667673031af8f22baa5e0ad7dda52568ac
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-11 23:44:51 +0200

    DealsSelect and Select update.
    Implement react-hook-form, and subscribe watching selected value from downshift's selects to the redux store.

--- src/components/molecules/DealsSelect/DealsSelect.js
+++ src/components/molecules/DealsSelect/DealsSelect.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useGetListCoveredRegionsQuery } from 'features/DealsApi/DealsApi'
 import Select from '../Select/Select'
 import { Wrapper } from './DealsSelect.style'
 import SelectSkeletonLoader from '../Select/SelectSkeletonLoader'
 import ErrorMessage from 'components/atoms/ErrorMessage/ErrorMessage'
+import { Controller, useForm, useWatch } from 'react-hook-form'
+import { takeSelections } from 'store'
+import { useDispatch } from 'react-redux'
+
 const DealsSelect = () => {
+  const { control } = useForm({
+    defaultValues: {
+      region: 'eu1',
+      country: 'AL',
+      shops: 'gog',
+      price: 'asc',
+    },
+  })
+
+  const dispatch = useDispatch()
   const { data, isLoading, error } = useGetListCoveredRegionsQuery()
+  const selectsWatch = useWatch({
+    control,
+  })
+  useEffect(() => {
+    dispatch(takeSelections(selectsWatch))
+  }, [selectsWatch, dispatch])
+
   return !error ? (
     <Wrapper>
       {isLoading
         ? Array(6)
             .fill('')
             .map((e, i) => <SelectSkeletonLoader key={i} />)
-        : data.map(({ name, options }) => <Select key={name} options={options} title={name} />)}
+        : data.map(({ name, options }) => (
+            <Controller
+              key={name}
+              control={control}
+              name={name.toLowerCase()}
+              render={({ field, onChange, value }) => <Select options={options} title={name} {...field} />}
+            />
+          ))}
     </Wrapper>
   ) : (
     <ErrorMessage>Something went wrong!</ErrorMessage>
--- src/components/molecules/Select/Select.js
+++ src/components/molecules/Select/Select.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { useSelect } from 'downshift'
 import { ReactComponent as Icon } from 'assets/icons/triangle-icon.svg'
 import { Wrapper, ToggleButton, SelectList, SelectListItem } from './Select.style'
-const Select = ({ options, title }) => {
-  const handleSelectedItemChange = () => {}
+const Select = React.forwardRef(({ options, title, onChange }, ref) => {
+  const handleSelectedItemChange = ({ selectedItem }) => {
+    onChange(selectedItem)
+  }
   const { selectedItem, isOpen, getToggleButtonProps, getMenuProps, highlightedIndex, getItemProps } = useSelect({
     items: options,
     onSelectedItemChange: handleSelectedItemChange,
   })
 
   return (
-    <Wrapper>
+    <Wrapper ref={ref}>
       <ToggleButton isOpen={isOpen} type="button" {...getToggleButtonProps()}>
         {(selectedItem && `${title}: ${selectedItem}`) || `${title} : ${options[0]}`}
         <Icon />
@@ -18,13 +20,12 @@ const Select = ({ options, title }) => {
       <SelectList isVisible={isOpen} {...getMenuProps()}>
         {isOpen &&
           options.map((item, index) => (
-            <SelectListItem isHighlited={highlightedIndex === index} key={`${item}${index}`} {...getItemProps({ item, index })}>
+            <SelectListItem value={item} isHighlited={highlightedIndex === index} key={`${item}${index}`} {...getItemProps({ item, index })}>
               {item}
             </SelectListItem>
           ))}
       </SelectList>
     </Wrapper>
   )
-}
-
+})
 export default Select
