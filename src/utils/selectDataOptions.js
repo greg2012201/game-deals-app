f913f80afcba9e002def850823a84ac09b3d3290
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-12 10:27:11 +0200

    selectDataOptions update
    Create the initialSelectsState object with the initial state values required for the DelsSelect and dealsSlice.

--- src/utils/selectDataOptions.js
+++ src/utils/selectDataOptions.js
@@ -1,4 +1,11 @@
 export const selectDataOptions = [
-  { name: 'Store', options: ['gog', 'steam'] },
+  { name: 'Shops', options: ['gog', 'steam', 'epic', 'battlenet'] },
   { name: 'Price', options: ['asc', 'desc'] },
 ]
+const transformedOptions = selectDataOptions.reduce((obj, { name, options }) => Object.assign(obj, { [name.toLowerCase()]: options[0] }), {})
+
+export const initialSelectsState = {
+  region: 'eu1',
+  country: 'AL',
+  ...transformedOptions,
+}
