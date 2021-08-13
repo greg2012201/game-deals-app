0ac495d0f70de82fa432a565a969e3399bd37e68
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-13 09:10:29 +0200

    Add RWD styles and edit the button style.

--- src/components/molecules/DealsListItem/DealsListItem.style.js
+++ src/components/molecules/DealsListItem/DealsListItem.style.js
@@ -2,50 +2,69 @@ import styled from 'styled-components'
 
 export const StyledListItem = styled.li`
   display: grid;
-  grid-template-rows: 30% 30px 23px 23px auto;
+  grid-template-rows: 30% 30px 23px 23px 23px auto;
   grid-template-columns: 55% 45%;
   padding: 10px;
   background-color: ${({ theme }) => theme.colors.darkerGrey};
   border-radius: 7px;
   color: ${({ theme }) => theme.colors.white};
   font-family: Lato;
-
+  min-height: 170px;
   p,
   a {
     margin: 0;
     padding: 0;
-    font-size: ${({ theme }) => theme.fontSize.l};
+    font-size: ${({ theme }) => theme.fontSize.m};
+    color: ${({ theme }) => theme.colors.lighterGrey};
   }
   p {
     margin-left: 4px;
+    grid-column: 1;
   }
   a {
-    color: ${({ theme }) => theme.colors.white};
-    grid-column: 2;
+    grid-column: 1;
+    grid-row: 6;
   }
 
-  h2 {
+  h3 {
     grid-column: 1/3;
     grid-row: 1/3;
+  }
+
+  @media (min-width: 660px) {
+    & {
+      display: flex;
+      flex-wrap: wrap;
+      min-height: 80px;
+      align-items: flex-end;
+      justify-content: space-between;
+    }
+    h3 {
+      width: 100%;
+      font-size: ${({ theme }) => theme.fontSize.x};
+    }
+    p,
+    a {
+      grid-row: 2;
+    }
   }
 `
 export const StyledAddButton = styled.button`
   box-sizing: border-box;
   grid-column: 2;
-  grid-row: span 3/5;
-  align-self: center;
+  grid-row: 5;
+  /* align-self: center; */
   justify-self: end;
   padding: 0;
   margin: 0;
   height: 40px;
   width: 110px;
   border: none;
-  background-color: ${({ theme }) => theme.colors.darkerGreen};
   border-radius: 7px;
   font-size: ${({ theme }) => theme.fontSize.xs};
   font-family: Krub;
   font-weight: 700;
-  color: ${({ theme }) => theme.colors.lightBlack};
+  color: ${({ theme }) => theme.colors.darkerGreen};
   cursor: pointer;
   &:hover {
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
--- src/components/oraganisms/DealsList/DealsList.style.js
+++ src/components/oraganisms/DealsList/DealsList.style.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.ul`
-  display: grid;
+  display: flex;
+  flex-direction: column;
+  /*  display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 330px));
-  grid-template-rows: repeat(auto-fill, minmax(150px, 180px));
-  justify-content: center;
+  justify-content: center; */
   grid-gap: 5px;
   margin: 0 auto;
   padding: 0;
+  max-width: 780px;
   width: 100%;
   list-style: none;
 `
