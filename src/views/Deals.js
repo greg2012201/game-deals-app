b38b815fd7663279156048218d1a9a67632986cc
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-13 09:11:42 +0200

    Add the RoundButton to the Deals view.

--- src/views/Deals.js
+++ src/views/Deals.js
@@ -1,11 +1,15 @@
+import RoundButton from 'components/atoms/RoundButton/RoundButton'
 import DealsList from 'components/oraganisms/DealsList/DealsList'
+import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo'
 import React from 'react'
 import { Wrapper } from './Deals.style'
 
 const Deals = () => {
+  const handleOnClick = () => customSmoothScrollTo()
   return (
     <Wrapper>
       <DealsList />
+      <RoundButton isReturn onClick={handleOnClick} />
     </Wrapper>
   )
 }
