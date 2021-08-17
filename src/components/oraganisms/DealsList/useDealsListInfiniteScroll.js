f5e4d24f0a85b045c74aec24e953902a57603a6e
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-17 14:07:31 +0200

    Implement the useDealsInfiniteScrollReducer.

--- src/components/oraganisms/DealsList/useDealsListInfiniteScroll.js
+++ src/components/oraganisms/DealsList/useDealsListInfiniteScroll.js
@@ -1,43 +1,48 @@
-import { useEffect, useState } from 'react'
-const initialState = { list: [], isLoading: true, isLoadingMore: false, hasMoreItems: true, currency: '', isError: false }
+import { useEffect } from 'react'
+import { useDealsListInfiniteScrollReducer } from './useDealsListInfiniteScrollReducer'
+const initialState = {
+  listSize: 0,
+  hasInitialLoader: true,
+  data: { list: [], isLoading: true, isLoadingMore: false, hasMoreItems: true, currency: '', isError: false },
+}
 export const useDealsListInfiniteScroll = ({ options, pageSize = 20, query }) => {
-  const [listSize, setListSize] = useState(0)
-  const [hasInitialLoader, setInitialLoader] = useState(true)
-  const [data, setData] = useState(initialState)
+  const {
+    state: { listSize, hasInitialLoader, data },
+    actionTypes: { incrementListSize, resetListSize, setInitialLoader, setData, setError },
+    dispatch,
+  } = useDealsListInfiniteScrollReducer({ initialState })
   const queryResult = query({ listSize: listSize + pageSize, options }, { skip: hasInitialLoader && listSize >= 20 })
   useEffect(() => {
     if (queryResult.isFetching) return
-    setInitialLoader(true)
-  }, [queryResult.isFetching])
+    return dispatch({ type: setInitialLoader, payload: true })
+  }, [queryResult.isFetching, dispatch, setInitialLoader])
 
   useEffect(() => {
-    return setListSize(0)
-  }, [options])
+    return dispatch({ type: resetListSize })
+  }, [options, dispatch, resetListSize])
   useEffect(() => {
     if (queryResult.isError) {
-      return setData({
-        ...initialState,
-        isError: queryResult.isError,
+      dispatch({ type: setError, payload: queryResult.isError })
+    }
+    if (queryResult.data) {
+      dispatch({
+        type: setData,
+        payload: {
+          list: queryResult.data.list,
+          isLoading: queryResult.isFetching && hasInitialLoader,
+          isLoadingMore: !queryResult.isFetching,
+          hasMoreItems: !queryResult.isLoading && queryResult.data.list.length <= queryResult.data.count,
+          currency: queryResult.data.currency,
+        },
       })
     }
-
-    return (
-      queryResult.data &&
-      setData({
-        list: queryResult.data.list,
-        isLoading: queryResult.isFetching && hasInitialLoader,
-        isLoadingMore: !queryResult.isFetching,
-        hasMoreItems: !queryResult.isLoading && queryResult.data.list.length <= queryResult.data.count,
-        currency: queryResult.data.currency,
-      })
-    )
-  }, [queryResult.data, hasInitialLoader, queryResult.isFetching, queryResult.isLoading, queryResult.isError])
+  }, [queryResult.data, hasInitialLoader, queryResult.isFetching, queryResult.isLoading, queryResult.isError, dispatch, setData, setError])
 
   const handleFetchMoreData = () => {
     if (queryResult.isLoading || queryResult.isFetching) return
-
-    setListSize((state) => (state += 20))
-    setInitialLoader(false)
+    dispatch({ type: incrementListSize, payload: pageSize })
+    dispatch({ type: setInitialLoader, payload: false })
   }
+
   return { handleFetchMoreData, data }
 }
