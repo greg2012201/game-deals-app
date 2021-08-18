33304c6120e80daa374e5c9e744372d666164687
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-18 09:19:43 +0200

    DealsList update.
    Implement a skeleton loader. Add spinner loader for the InfiniteScroll also add the ErrorPage.

--- src/components/oraganisms/DealsList/DealsList.js
+++ src/components/oraganisms/DealsList/DealsList.js
@@ -5,26 +5,41 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 import { StyledEndMessage } from '../GamesList/GamesList.style'
 import { useDealsListInfiniteScroll } from 'components/oraganisms/DealsList/useDealsListInfiniteScroll'
 import { useGetDealsListQuery } from 'features/DealsApi/DealsApi'
+import DealsListItemSkeletonLoader from 'components/molecules/DealsListItem/DealsListItemSkeletonLoader'
+import Loader from 'react-loader-spinner'
+import { useTheme } from 'styled-components'
+import ErrorPage from 'components/molecules/ErrorPage/ErrorPage'
 
 const DealsList = () => {
+  const theme = useTheme()
   const options = useSelector((state) => state.dealsListOptions)
   const { handleFetchMoreData, data } = useDealsListInfiniteScroll({ options, query: useGetDealsListQuery })
   return data.isLoading ? (
-    <h1>LOADER</h1>
+    data.isError ? (
+      <ErrorPage>Something went wrong, we couldn't load your content</ErrorPage>
+    ) : (
+      <Wrapper>
+        {Array(20)
+          .fill('')
+          .map((e, i) => (
+            <DealsListItemSkeletonLoader key={i} />
+          ))}
+      </Wrapper>
+    )
   ) : (
     <InfiniteScroll
-      scrollThreshold={'500px'}
       dataLength={data.list.length}
       next={handleFetchMoreData}
       hasMore={data.hasMoreItems}
       endMessage={data.isLoadingMore && <StyledEndMessage style={{ textAlign: 'center' }}>this is the end</StyledEndMessage>}
-      loader={'loading'}
+      loader={!data.isError && <Loader type="ThreeDots" color={`${theme.colors.white}`} style={{ textAlign: 'center' }} />}
     >
       <Wrapper>
         {data.list.map((item, i) => (
           <DealsListItem currency={data.currency} data={item} key={i} />
         ))}
       </Wrapper>
+      )
     </InfiniteScroll>
   )
 }
