59000d478ab2e3522c52c7e942356385cc4abe73
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-20 10:07:27 +0200

    Add the redux-firestore features to the store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { dealsApi } from 'features/DealsApi/DealsApi'
 import { dealsListOptionsSlice } from 'features/DealsOptionsSlice/DealsListOptionsSlice'
-
+import 'firebase/firestore'
+import { firestoreReducer } from 'redux-firestore'
 export const store = configureStore({
   reducer: {
     [dealsApi.reducerPath]: dealsApi.reducer,
     dealsListOptions: dealsListOptionsSlice.reducer,
+    firestore: firestoreReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(dealsApi.middleware),
 })
