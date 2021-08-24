d67202c754127ad48a68896957dd29b69043c4cb
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-24 10:45:58 +0200

    Add titles & delete StyledLinkButton.

--- src/views/Deals.js
+++ src/views/Deals.js
@@ -3,11 +3,12 @@ import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo'
 
 import React from 'react'
 import { pathsList } from 'routes'
-import { StyledLinkButton, Wrapper } from './Deals.style'
 import WishList from 'components/oraganisms/WishList/WishList'
 import { Route } from 'react-router-dom'
 import { useFirestoreConnect } from 'react-redux-firebase'
 import InfiniteDealsList from 'components/oraganisms/InfiniteDealsList/InfiniteDealsList'
+import { Wrapper } from './Deals.style'
+import Title from 'components/atoms/Title/Title'
 
 const { deals, whishList } = pathsList
 const Deals = () => {
@@ -17,10 +18,11 @@ const Deals = () => {
   return (
     <Wrapper>
       <Route exact path={`${deals}`}>
-        <StyledLinkButton to={`${deals}${whishList}`}>Go to whishlist </StyledLinkButton>
+        <Title titleType="h1">Deals</Title>
         <InfiniteDealsList />
       </Route>
       <Route path={`${deals}${whishList}`}>
+        <Title titleType="h1">WishList</Title>
         <WishList />
       </Route>
       <RoundButton isReturn onClick={handleOnClick} />
