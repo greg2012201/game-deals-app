87b6418aa4c7d143a65b16555281f16a600a672f
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-24 14:05:13 +0200

    DealsList update.
    Add the skeleton loader and error page. Remove the currency prop form DealsListItem. Edit DealsList given props.

--- src/components/oraganisms/DealsList.js/DealsList.js
+++ src/components/oraganisms/DealsList.js/DealsList.js
@@ -1,18 +1,25 @@
 import DealsListItem from 'components/molecules/DealsListItem/DealsListItem'
+import DealsListItemSkeletonLoader from 'components/molecules/DealsListItem/DealsListItemSkeletonLoader'
+import ErrorPage from 'components/molecules/ErrorPage/ErrorPage'
 import React from 'react'
 import { Wrapper } from './DealsList.style'
-const DealsList = ({ data, isWishList = false, handleOnClick, isButtonRemove, updatedPrice = null }) => {
-  return (
+const DealsList = ({ data, isWishList = false, handleOnClick, updatedPrice = null }) => {
+  return data.isLoading ? (
+    data.isError ? (
+      <ErrorPage>Something went wrong, we couldn't load your content</ErrorPage>
+    ) : (
+      <Wrapper>
+        {Array(20)
+          .fill('')
+          .map((e, i) => (
+            <DealsListItemSkeletonLoader key={i} />
+          ))}
+      </Wrapper>
+    )
+  ) : (
     <Wrapper>
       {data.list.map((item, i) => (
-        <DealsListItem
-          handleOnClick={handleOnClick}
-          isWishList={isWishList}
-          currency={data.currency}
-          data={item}
-          key={i}
-          updatedPrice={updatedPrice}
-        />
+        <DealsListItem handleOnClick={handleOnClick} isWishList={isWishList} data={item} key={i} updatedPrice={updatedPrice} />
       ))}
     </Wrapper>
   )
