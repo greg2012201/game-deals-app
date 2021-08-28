1282d8ed2b038db14f5e43eb0700fb6276e9527e
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-28 14:59:59 +0200

    Add missing semicolons.

--- src/components/oraganisms/WishList/useWishList.js
+++ src/components/oraganisms/WishList/useWishList.js
@@ -1,50 +1,49 @@
-import { useEffect } from 'react'
-import { useGetActualPricesQuery } from 'features/WishListApi/WishListApi'
-import { useWishListStateMachine } from './useWishListStateMachine'
-import { useWishListFirestore } from './useWishListFirestore'
+import { useEffect } from 'react';
+import { useGetActualPricesQuery } from 'features/WishListApi/WishListApi';
+import { useWishListStateMachine } from './useWishListStateMachine';
+import { useWishListFirestore } from './useWishListFirestore';
 
 export const useWishList = () => {
   const {
     dispatch,
     actionTypes,
     state: { hasLoader },
-  } = useWishListStateMachine()
-  const { wishList, addToStore, removeFromStore, removeAllFromStore, isItemSwitching, error } = useWishListFirestore()
-  const { data, isLoading } = useGetActualPricesQuery({ plains: wishList }, { skip: !wishList || wishList.length === 0 || !hasLoader })
+  } = useWishListStateMachine();
+  const { wishList, addToStore, removeFromStore, removeAllFromStore, isItemSwitching, error } = useWishListFirestore();
+  const { data, isLoading } = useGetActualPricesQuery({ plains: wishList }, { skip: !wishList || wishList.length === 0 || !hasLoader });
   useEffect(() => {
-    if (isLoading || !wishList) return
-    dispatch({ type: actionTypes.hasListLoaded })
+    if (isLoading || !wishList) return;
+    dispatch({ type: actionTypes.hasListLoaded });
     return () => {
-      return dispatch({ type: actionTypes.hasListLoaded })
-    }
-  }, [isLoading, wishList, dispatch, actionTypes.hasListLoaded])
+      return dispatch({ type: actionTypes.hasListLoaded });
+    };
+  }, [isLoading, wishList, dispatch, actionTypes.hasListLoaded]);
 
   const handleOnClick = ({ isWishList, id, data }) => {
     if (isWishList) {
-      return removeFromStore(id)
+      return removeFromStore(id);
     } else {
-      return toggleItemInStore(data)
+      return toggleItemInStore(data);
     }
-  }
+  };
   const findDuplicatedItemsByPlains = (plain, items) => {
     return items.find((obj) => {
-      return obj.plain === plain
-    })
-  }
+      return obj.plain === plain;
+    });
+  };
   const compareItemsPriceByPlain = (plain) => {
-    const transformedData = data.actualPrices.find((e) => e.plain === plain)
-    return transformedData
-  }
+    const transformedData = data.actualPrices.find((e) => e.plain === plain);
+    return transformedData;
+  };
 
   const toggleItemInStore = (selectedDeal) => {
-    const foundItemInStore = findDuplicatedItemsByPlains(selectedDeal.plain, wishList)
+    const foundItemInStore = findDuplicatedItemsByPlains(selectedDeal.plain, wishList);
     if (!foundItemInStore) {
-      addToStore(selectedDeal)
+      addToStore(selectedDeal);
     } else {
-      removeFromStore(foundItemInStore.id)
+      removeFromStore(foundItemInStore.id);
     }
-  }
-
+  };
   return {
     data: { list: wishList, isLoading: hasLoader },
     compareItemsPriceByPlain,
@@ -54,5 +53,5 @@ export const useWishList = () => {
     findDuplicatedItemsByPlains,
     isItemSwitching,
     error,
-  }
-}
+  };
+};
