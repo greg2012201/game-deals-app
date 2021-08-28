fb19bae8675c72508e33d2818b4c3cfe424ca9e7
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-28 15:04:18 +0200

    update useWishListFirestore.
    Delete the useFirestoreConnect call & add missing semi-colons.

--- src/components/oraganisms/WishList/useWishListFirestore.js
+++ src/components/oraganisms/WishList/useWishListFirestore.js
@@ -1,50 +1,48 @@
-import { useFirestore } from 'react-redux-firebase'
-import { useSelector } from 'react-redux'
-import { useWishListStateMachine } from './useWishListStateMachine'
-import { useFirestoreConnect } from 'react-redux-firebase'
+import { useFirestore } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
+import { useWishListStateMachine } from './useWishListStateMachine';
 export const useWishListFirestore = () => {
-  useFirestoreConnect('wishList')
-  const firestore = useFirestore()
-  const collection = firestore.collection('wishList')
-  const wishList = useSelector((state) => state.firestore.ordered.wishList)
+  const firestore = useFirestore();
+  const collection = firestore.collection('wishList');
+  const wishList = useSelector((state) => state.firestore.ordered.wishList);
   const {
     dispatch,
     actionTypes,
     state: { isItemSwitching },
     error,
-  } = useWishListStateMachine()
+  } = useWishListStateMachine();
 
   const removeFromStore = async (id) => {
-    dispatch({ type: actionTypes.switchItem })
+    dispatch({ type: actionTypes.switchItem });
     try {
-      await collection.doc(id).delete()
-      return dispatch({ type: actionTypes.success })
+      await collection.doc(id).delete();
+      return dispatch({ type: actionTypes.success });
     } catch (err) {
-      return dispatch({ type: actionTypes.error })
+      return dispatch({ type: actionTypes.error });
     }
-  }
+  };
   const removeAllFromStore = async () => {
-    dispatch({ type: actionTypes.switchItem })
-    const batch = firestore.batch()
+    dispatch({ type: actionTypes.switchItem });
+    const batch = firestore.batch();
 
     try {
-      const items = (await collection.get()).docs
-      items.map((item) => batch.delete(item.ref))
-      await batch.commit()
-      return dispatch({ type: actionTypes.success })
+      const items = (await collection.get()).docs;
+      items.map((item) => batch.delete(item.ref));
+      await batch.commit();
+      return dispatch({ type: actionTypes.success });
     } catch (err) {
-      return dispatch({ type: actionTypes.error })
+      return dispatch({ type: actionTypes.error });
     }
-  }
+  };
   const addToStore = async (payload) => {
-    dispatch({ type: actionTypes.switchItem })
+    dispatch({ type: actionTypes.switchItem });
     try {
-      await collection.add(payload)
-      return dispatch({ type: actionTypes.success })
+      await collection.add(payload);
+      return dispatch({ type: actionTypes.success });
     } catch (err) {
-      return dispatch({ type: actionTypes.error })
+      return dispatch({ type: actionTypes.error });
     }
-  }
+  };
 
-  return { wishList, addToStore, removeFromStore, removeAllFromStore, isItemSwitching, error }
-}
+  return { wishList, addToStore, removeFromStore, removeAllFromStore, isItemSwitching, error };
+};
