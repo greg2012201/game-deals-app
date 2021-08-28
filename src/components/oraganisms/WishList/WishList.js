feacd85d105b57ec4f7f73eee9d9071e328be920
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-28 15:05:25 +0200

    WishList update.
    Add the DeleteBinButton & DialogModal. Add missing semi-colons.

--- src/components/oraganisms/WishList/WishList.js
+++ src/components/oraganisms/WishList/WishList.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import DealsList from '../DealsList.js/DealsList'
-import { useWishList } from './useWishList'
-
+import DeleteBinButton from 'components/atoms/DeleteBinButton/DeleteBinButton';
+import Title from 'components/atoms/Title/Title';
+import DialogModal from 'components/molecules/DialogModal/DialogModal';
+import { useDialogModal } from 'components/molecules/DialogModal/useDialogModal';
+import React from 'react';
+import DealsList from '../DealsList.js/DealsList';
+import { useWishList } from './useWishList';
 const WishList = () => {
-  const { data } = useWishList()
+  const { data, removeAllFromStore, isItemSwitching } = useWishList();
+  const { openModal, modalIsOpen, closeModal, handleOnClick } = useDialogModal();
+  return (
+    <>
+      <Title titleType="h1">WishList</Title>
 
-  return <DealsList isWishList data={data} />
-}
+      <DeleteBinButton onClick={openModal} isVisible={true} isLoading={isItemSwitching} />
+      <DialogModal
+        title={'Delete items'}
+        handleOnClick={handleOnClick}
+        closeModal={closeModal}
+        isOpen={modalIsOpen}
+        handleOnConsent={removeAllFromStore}
+      >
+        Are you sure you want to remove all items from WishList ?
+      </DialogModal>
 
-export default WishList
+      <DealsList isWishList data={data} />
+    </>
+  );
+};
+export default WishList;
