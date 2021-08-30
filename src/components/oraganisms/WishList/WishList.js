ba16332b9e14ccf7e8bd70dfd04c5c5cbdb34982
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-30 09:08:42 +0200

    Update the WishList.
    Call the useDeleteBinButtonAnimation. Edit DeleteBinButton's props.Create the debouncedRemove function thats  depends on  the animation of the delete bin button.

--- src/components/oraganisms/WishList/WishList.js
+++ src/components/oraganisms/WishList/WishList.js
@@ -1,27 +1,36 @@
 import DeleteBinButton from 'components/atoms/DeleteBinButton/DeleteBinButton';
+import { useDeleteBinButtonAnimation } from 'components/atoms/DeleteBinButton/useDeleteBinButtonAnimation';
 import Title from 'components/atoms/Title/Title';
 import DialogModal from 'components/molecules/DialogModal/DialogModal';
 import { useDialogModal } from 'components/molecules/DialogModal/useDialogModal';
-import React from 'react';
+import debounce from 'lodash.debounce';
+import React, { useMemo } from 'react';
 import DealsList from '../DealsList.js/DealsList';
 import { useWishList } from './useWishList';
 const WishList = () => {
-  const { data, removeAllFromStore, isItemSwitching } = useWishList();
+  const { data, removeAllFromStore } = useWishList();
   const { openModal, modalIsOpen, closeModal, handleOnClick } = useDialogModal();
+  const { hasAnimationStarted, startAnimation, delay } = useDeleteBinButtonAnimation();
+  const debouncedRemove = useMemo(() => {
+    return debounce(removeAllFromStore, delay);
+  }, [removeAllFromStore, delay]);
   return (
     <>
       <Title titleType="h1">WishList</Title>
-
-      <DeleteBinButton onClick={openModal} isVisible={true} isLoading={isItemSwitching} />
-      <DialogModal
-        title={'Delete items'}
-        handleOnClick={handleOnClick}
-        closeModal={closeModal}
-        isOpen={modalIsOpen}
-        handleOnConsent={removeAllFromStore}
-      >
-        Are you sure you want to remove all items from WishList ?
-      </DialogModal>
+      {data.list && data.list.length !== 0 ? (
+        <>
+          <DeleteBinButton onClick={openModal} hasAnimationStarted={hasAnimationStarted} />
+          <DialogModal
+            title={'Delete items'}
+            handleOnClick={handleOnClick}
+            closeModal={closeModal}
+            isOpen={modalIsOpen}
+            handleOnConsent={[debouncedRemove, startAnimation]}
+          >
+            Are you sure you want to remove all items from WishList ?
+          </DialogModal>
+        </>
+      ) : null}
 
       <DealsList isWishList data={data} />
     </>
