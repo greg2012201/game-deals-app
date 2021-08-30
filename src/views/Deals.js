b18a9c9873d7281faf26f01ea45e356a24488639
Grzegorz Dubiel <greg2012201@gmail.com>
2021-08-30 09:09:17 +0200

    Create the DealsTemplate and add it to the Deals view.

--- src/views/Deals.js
+++ src/views/Deals.js
@@ -1,30 +1,34 @@
-import RoundButton from 'components/atoms/RoundButton/RoundButton'
-import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo'
+import RoundButton from 'components/atoms/RoundButton/RoundButton';
+import { customSmoothScrollTo } from 'helpers/customSmoothScrollTo';
 
-import React from 'react'
-import { pathsList } from 'routes'
-import WishList from 'components/oraganisms/WishList/WishList'
-import { Route } from 'react-router-dom'
-import InfiniteDealsList from 'components/oraganisms/InfiniteDealsList/InfiniteDealsList'
-import { Wrapper } from './Deals.style'
-import Title from 'components/atoms/Title/Title'
-const { deals, wishList } = pathsList
+import React from 'react';
+import { pathsList } from 'routes';
+import WishList from 'components/oraganisms/WishList/WishList';
+import { Route } from 'react-router-dom';
+import InfiniteDealsList from 'components/oraganisms/InfiniteDealsList/InfiniteDealsList';
+import { Wrapper } from './Deals.style';
+import Title from 'components/atoms/Title/Title';
+import { useFirestoreConnect } from 'react-redux-firebase';
+import { DealsTemplate } from 'components/templates/DealsTemplate/DealsTemplate';
+const { deals, wishList } = pathsList;
 const Deals = () => {
-  const handleOnClick = () => customSmoothScrollTo()
+  const handleOnClick = () => customSmoothScrollTo();
+  useFirestoreConnect('wishList');
 
   return (
     <Wrapper>
-      <Route exact path={`${deals}`}>
-        <Title titleType="h1">Deals</Title>
-        <InfiniteDealsList />
-      </Route>
-      <Route path={`${deals}${wishList}`}>
-        <Title titleType="h1">WishList</Title>
-        <WishList />
-      </Route>
-      <RoundButton isReturn onClick={handleOnClick} />
+      <DealsTemplate>
+        <Route exact path={`${deals}`}>
+          <Title titleType="h1">Deals</Title>
+          <InfiniteDealsList />
+        </Route>
+        <Route exact path={`${deals}${wishList}`}>
+          <WishList />
+        </Route>
+        <RoundButton isReturn onClick={handleOnClick} />
+      </DealsTemplate>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Deals
+export default Deals;
