27508353b229c537997908a61d7f4258b240cef3
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-04 17:50:39 +0200

    update SearchBar & useGetMachingGames
    Add onClick prop to the input element. Rename wrong actions vatriable. Add missing semi-colons.

--- src/components/oraganisms/SearchBar/SearchBar.js
+++ src/components/oraganisms/SearchBar/SearchBar.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { ReactComponent as Icon } from 'assets/icons/magnifier-icon.svg'
-import { Hint, HintWrapper, StyledLoader, StyledResetRoundButton, Wrapper } from './SearchBar.style'
-import { useCombobox } from 'downshift'
-import { states } from 'utils/state/states'
-import { useTheme } from 'styled-components'
-import { useSearchBar } from './useSearchBar'
-import Title from 'components/atoms/Title/Title'
-import ErrorMessage from 'components/atoms/ErrorMessage/ErrorMessage'
+import React from 'react';
+import { ReactComponent as Icon } from 'assets/icons/magnifier-icon.svg';
+import { Hint, HintWrapper, StyledLoader, StyledResetRoundButton, Wrapper } from './SearchBar.style';
+import { useCombobox } from 'downshift';
+import { states } from 'utils/state/states';
+import { useTheme } from 'styled-components';
+import { useSearchBar } from './useSearchBar';
+import Title from 'components/atoms/Title/Title';
+import ErrorMessage from 'components/atoms/ErrorMessage/ErrorMessage';
 const SearchBar = () => {
-  const theme = useTheme()
+  const theme = useTheme();
 
   const {
     handleItemToString,
@@ -18,21 +18,22 @@ const SearchBar = () => {
     compareSearchState,
     machingGames,
     error,
-  } = useSearchBar()
-  const { isOpen, inputValue, getMenuProps, getInputProps, getComboboxProps, getItemProps, reset, highlightedIndex } = useCombobox({
+  } = useSearchBar();
+
+  const { isOpen, openMenu, inputValue, getMenuProps, getInputProps, getComboboxProps, getItemProps, reset, highlightedIndex } = useCombobox({
     items: machingGames.results ? machingGames.results : [],
     onInputValueChange: handleOnInputValueChange,
     itemToString: handleItemToString,
     onSelectedItemChange: handleOnSelectedItemChange,
-  })
+  });
   return (
     <Wrapper {...getComboboxProps()}>
+      {' '}
       <label htmlFor="search">
         <Icon />
       </label>
-      <input {...getInputProps()} id="search" name="search" placeholder="Search for game details..." />
+      <input {...getInputProps()} id="search" name="search" placeholder="Search for game details..." onClick={openMenu} />
       <StyledResetRoundButton isReset isVisible={inputValue} onClick={() => reset()} />
-
       {!compareFetchstate(states.hasError) ? (
         <HintWrapper isEmpty={!machingGames.results && !compareFetchstate(states.isLoading)} isVisible={isOpen} {...getMenuProps()}>
           {isOpen ? (
@@ -51,7 +52,7 @@ const SearchBar = () => {
                     <img src={background_image} alt={name} />
                     <Title titleType="h4">{name}</Title>
                   </Hint>
-                )
+                );
               })
             ) : (
               <StyledLoader className="loader" type="Oval" color={theme.colors.darkWhite} height={40} width={40} />
@@ -64,7 +65,7 @@ const SearchBar = () => {
         </HintWrapper>
       )}
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- src/components/oraganisms/SearchBar/useGetMachingGames.js
+++ src/components/oraganisms/SearchBar/useGetMachingGames.js
@@ -1,19 +1,19 @@
-import { useFetchData } from 'hooks/useFetchData'
-import debounce from 'lodash.debounce'
-import { useMemo } from 'react'
-import { RAWGOptions } from 'utils/fetchingOptions'
-import { actions } from 'utils/state/transitions'
-const { url, key } = RAWGOptions
+import { useFetchData } from 'hooks/useFetchData';
+import debounce from 'lodash.debounce';
+import { useMemo } from 'react';
+import { RAWGOptions } from 'utils/fetchingOptions';
+import { actions } from 'utils/state/transitions';
+const { url, key } = RAWGOptions;
 
 export const useGetMachingGames = (updateSearchState, setMachingGames) => {
-  const { fetchData, compareState: compareFetchstate, error } = useFetchData(setMachingGames)
+  const { fetchData, compareState: compareFetchstate, error } = useFetchData(setMachingGames);
   const getMachingGames = useMemo(() => {
-    updateSearchState(actions.fetch)
+    updateSearchState(actions.initialize);
     return debounce((inputValue) => {
-      fetchData({ url: `${url}/games?search=303%20${inputValue}&key=${key}` })
-      updateSearchState(actions.success)
-    }, 200)
-  }, [fetchData, updateSearchState])
+      fetchData({ url: `${url}/games?search=303%20${inputValue}&key=${key}` });
+      updateSearchState(actions.success);
+    }, 200);
+  }, [fetchData, updateSearchState]);
 
-  return { getMachingGames, compareFetchstate, error }
-}
+  return { getMachingGames, compareFetchstate, error };
+};
