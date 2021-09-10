3d5936078f828b023b6e434c7ae730ba981bd87b
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-10 23:59:50 +0200

    Rename the pageSize variable & reformat code.

--- src/components/oraganisms/InfiniteDealsList/InfiniteDealsList.js
+++ src/components/oraganisms/InfiniteDealsList/InfiniteDealsList.js
@@ -1,4 +1,5 @@
 import InfiniteScroll from 'react-infinite-scroll-component';
+
 import { StyledEndMessage } from '../GamesList/GamesList.style';
 import { useSelector } from 'react-redux';
 import Loader from 'react-loader-spinner';
@@ -8,13 +9,13 @@ import { useDealsListInfiniteScroll } from './useDealsListInfiniteScroll';
 import { useGetDealsListQuery } from 'features/DealsApi/DealsApi';
 import DealsSelect from 'components/molecules/DealsSelect/DealsSelect';
 import { useWishListFirestore } from '../WishList/useWishListFirestore';
-const pageSize = 40;
+const itemsPerLoad = 40;
 const InfiniteDealsList = () => {
-  const theme = useTheme();
   const options = useSelector((state) => state.dealsListOptions);
+  const theme = useTheme();
   const { handleOnClick, findItemsInWishListFirestore, isLoading } = useWishListFirestore();
   const { handleFetchMoreData, data } = useDealsListInfiniteScroll({
-    pageSize,
+    itemsPerLoad,
     options,
     query: useGetDealsListQuery,
   });
@@ -26,10 +27,27 @@ const InfiniteDealsList = () => {
         dataLength={data.list.length}
         next={handleFetchMoreData}
         hasMore={data.hasMoreItems}
-        endMessage={data.isLoadingMore && <StyledEndMessage style={{ textAlign: 'center' }}>this is the end</StyledEndMessage>}
-        loader={!data.isError && <Loader type="ThreeDots" color={`${theme.colors.white}`} style={{ textAlign: 'center' }} />}
+        endMessage={
+          data.isLoadingMore && (
+            <StyledEndMessage style={{ textAlign: 'center' }}>this is the end</StyledEndMessage>
+          )
+        }
+        loader={
+          !data.isError && (
+            <Loader
+              type="ThreeDots"
+              color={`${theme.colors.white}`}
+              style={{ textAlign: 'center' }}
+            />
+          )
+        }
       >
-        <DealsList data={data} handleOnClick={handleOnClick} firestoreConnecting={isLoading} checkIsOnWishList={findItemsInWishListFirestore} />
+        <DealsList
+          data={data}
+          handleOnClick={handleOnClick}
+          firestoreConnecting={isLoading}
+          checkIsOnWishList={findItemsInWishListFirestore}
+        />
       </InfiniteScroll>
     </>
   );
