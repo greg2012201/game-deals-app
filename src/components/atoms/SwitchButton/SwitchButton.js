a72162182121f8ae9ade7a91cf0782afe20ceee5
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-15 10:29:35 +0200

    Update the SwitchButton.
    Add the tooltip. Move text from the button to the tooltip.

--- src/components/atoms/SwitchButton/SwitchButton.js
+++ src/components/atoms/SwitchButton/SwitchButton.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { usePopperTooltip } from 'react-popper-tooltip';
+import Tooltip from '../Tooltip/Tooltip';
 import { StyledAddButton } from './SwitchButton.style';
 
 const SwitchButton = (props) => {
-  return <StyledAddButton {...props}>{props.isRemove ? 'Remove ' : 'Add to wish list'}</StyledAddButton>;
+  const {
+    getArrowProps,
+    getTooltipProps,
+    setTriggerRef,
+    setTooltipRef,
+    visible,
+  } = usePopperTooltip({ placement: 'top', offset: [0, 10] });
+  return (
+    <StyledAddButton ref={setTriggerRef} {...props}>
+      <Tooltip popperProps={{ getTooltipProps, getArrowProps, setTooltipRef, visible }}>
+        {props.isRemove ? 'Delete' : 'Add'}
+      </Tooltip>
+    </StyledAddButton>
+  );
 };
 
 export default SwitchButton;
