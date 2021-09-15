107a1e2b51dcada132b8567603446438ea88215b
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-15 09:40:26 +0200

    PCRequirements update.
    Fix the bug with crashing the component when data.platform is undefined.

--- src/components/molecules/PCRequirements/PCRequirements.js
+++ src/components/molecules/PCRequirements/PCRequirements.js
@@ -1,34 +1,35 @@
-import { Paragraph } from 'components/atoms/Paragraph/Paragraph'
-import Title from 'components/atoms/Title/Title'
-import { useGameDetails } from 'hooks/useGameDetails'
-import React from 'react'
-import { states } from 'utils/state/states'
-import TextContainer from '../TextContainer/TextContainer'
-import { RequirementsWrapper } from './PCRequirements.style'
+import { Paragraph } from 'components/atoms/Paragraph/Paragraph';
+import Title from 'components/atoms/Title/Title';
+import { useGameDetails } from 'hooks/useGameDetails';
+import React from 'react';
+import { states } from 'utils/state/states';
+import TextContainer from '../TextContainer/TextContainer';
+import { RequirementsWrapper } from './PCRequirements.style';
 
 const PCRequirements = () => {
-  const { data, compareState } = useGameDetails()
+  const { data, compareState } = useGameDetails();
 
   return (
     !compareState(states.idle) && (
       <RequirementsWrapper>
         <Title titleType="h2">System requirements</Title>
-        {data.platforms.map(({ platform, requirements }, index) => {
-          return (
-            platform.id === 4 &&
-            (Object.keys(requirements).length ? (
-              Object.keys(requirements).map((key, i) => (
-                <TextContainer key={i} viewHeight={40}>
-                  {requirements[key]}
-                </TextContainer>
+        {data.platforms &&
+          data.platforms.map(({ platform, requirements }, index) => {
+            return (
+              platform.id === 4 &&
+              (Object.keys(requirements).length ? (
+                Object.keys(requirements).map((key, i) => (
+                  <TextContainer key={i} viewHeight={40}>
+                    {requirements[key]}
+                  </TextContainer>
+                ))
+              ) : (
+                <Paragraph key={index}>There is nothing here</Paragraph>
               ))
-            ) : (
-              <Paragraph key={index}>There is nothing here</Paragraph>
-            ))
-          )
-        })}
+            );
+          })}
       </RequirementsWrapper>
     )
-  )
-}
-export default PCRequirements
+  );
+};
+export default PCRequirements;
