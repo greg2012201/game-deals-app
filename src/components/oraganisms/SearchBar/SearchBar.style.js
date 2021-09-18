5abdf1457a2aa85b8c85b601518a92aff8a68168
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-18 22:56:01 +0200

    Create the Input component.
    Replace plain inputs with Input component in SearchBar.

--- src/components/oraganisms/SearchBar/SearchBar.style.js
+++ src/components/oraganisms/SearchBar/SearchBar.style.js
@@ -1,3 +1,4 @@
+import { Input } from 'components/atoms/Input/Input';
 import RoundButton from 'components/atoms/RoundButton/RoundButton';
 import Loader from 'react-loader-spinner';
 import styled from 'styled-components';
@@ -20,37 +21,11 @@ export const Wrapper = styled.div`
       rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
       rgba(0, 0, 0, 0.09) 0px -3px 5px;
   }
-  input {
-    height: 33px;
+  ${Input} {
     border-radius: 0px 7px 7px 0px;
-    border: none;
-    font-family: Krub;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 13px;
-    line-height: 17px;
-    display: flex;
-    align-items: center;
     width: 95%;
-    background-color: ${({ theme }) => theme.colors.black};
-    width: 100%;
-    color: ${({ theme }) => theme.colors.white};
   }
-  input:-webkit-autofill,
-  input:-webkit-autofill:hover,
-  input:-webkit-autofill:focus,
-  textarea:-webkit-autofill,
-  textarea:-webkit-autofill:hover,
-  textarea:-webkit-autofill:focus,
-  select:-webkit-autofill,
-  select:-webkit-autofill:hover,
-  select:-webkit-autofill:focus {
-    -webkit-text-fill-color: ${({ theme }) => theme.colors.white};
-    -webkit-box-shadow: 0 0 0px 1000px ${({ theme }) => theme.colors.black} inset;
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
-      rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
-      rgba(0, 0, 0, 0.09) 0px -3px 5px;
-  }
+
   label {
     padding: 0;
     display: flex;
