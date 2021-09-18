6bf2bf63099fbaecd75d0172dec264eff6f70b3e
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-18 23:03:15 +0200

    Add the quick return to the useEffect hook.

--- src/hooks/usePanelVisibilityToggle.js
+++ src/hooks/usePanelVisibilityToggle.js
@@ -3,8 +3,14 @@ import { useEffect, useState } from 'react';
 export const usePanelVisibilityToggle = (changingElement, targetElement) => {
   const [visibility, setVisibility] = useState(true);
   useEffect(() => {
+    if (!targetElement || !changingElement || !targetElement.current || !changingElement.current)
+      return;
     const isTarget = () => {
-      setVisibility(targetElement.current.getBoundingClientRect().top + targetElement.current.getBoundingClientRect().height >= 0);
+      setVisibility(
+        targetElement.current.getBoundingClientRect().top +
+          targetElement.current.getBoundingClientRect().height >=
+          0
+      );
     };
     if (changingElement && targetElement) {
       isTarget();
