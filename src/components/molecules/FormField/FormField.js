2bbf5e65efca74dbfa8b54068c034b993f809217
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-21 10:33:36 +0200

    Add the register prop form the react-hook-form.

--- src/components/molecules/FormField/FormField.js
+++ src/components/molecules/FormField/FormField.js
@@ -2,7 +2,7 @@ import { Input } from 'components/atoms/Input/Input';
 import React, { useState } from 'react';
 import { StyledLabel, Wrapper } from './FormField.style';
 
-const FormField = ({ children, name, type = 'text' }) => {
+const FormField = ({ children, name, type = 'text', register }) => {
   const [hasFocus, setHasFocus] = useState(false);
   const toggleFocus = () => {
     return setHasFocus((state) => !state);
@@ -20,6 +20,7 @@ const FormField = ({ children, name, type = 'text' }) => {
         onBlur={toggleFocus}
         onFocus={toggleFocus}
         autoComplete="on"
+        {...register(`${name}`, { required: true })}
       />
     </Wrapper>
   );
