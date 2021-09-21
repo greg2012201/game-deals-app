3a229cf83aaa34846beb9ecb8412c987323203ac
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-21 13:03:53 +0200

    Set up the auth conditions for the router.

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -1,8 +1,20 @@
 import AuthForm from 'components/oraganisms/AuthFrorm/AuthForm';
 import { Wrapper } from './LoginPage.style';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { pathsList } from 'routes';
+import { useSelector } from 'react-redux';
 
+const { deals, wishList } = pathsList;
 const LoginPage = () => {
+  const history = useHistory();
+  const auth = useSelector((state) => state.firebase.auth);
+  useEffect(() => {
+    if (auth.isLoaded && !auth.isEmpty) {
+      history.push(`${deals}${wishList}`);
+    }
+  }, [history, auth.isLoaded, auth.isEmpty]);
+
   return (
     <Wrapper>
       <AuthForm />
