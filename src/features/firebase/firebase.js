2133c5cea6121c13eaabc8fcffae282b987a4a1f
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-21 10:40:11 +0200

    Adjust the configuration to the auth.

--- src/features/firebase/firebase.js
+++ src/features/firebase/firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app'
-import { createFirestoreInstance } from 'redux-firestore'
-import { store } from 'store'
+import firebase from 'firebase/app';
+import { createFirestoreInstance } from 'redux-firestore';
+import { store } from 'store';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'games-deals.firebaseapp.com',
@@ -8,15 +8,15 @@ const firebaseConfig = {
   storageBucket: 'games-deals.appspot.com',
   messagingSenderId: '981375237692',
   appId: '1:981375237692:web:6b8894b7742a2624e78d39',
-}
-firebase.initializeApp(firebaseConfig)
-firebase.firestore()
+};
+firebase.initializeApp(firebaseConfig);
 const rrfConfig = {
+  userProfile: 'users',
   useFirestoreForProfile: true,
-}
+};
 export const rrfProps = {
   firebase,
   config: rrfConfig,
   dispatch: store.dispatch,
   createFirestoreInstance,
-}
+};
