4d620721dec8f4ff1ed79e18ab11055d99499a0f
Grzegorz Dubiel <greg2012201@gmail.com>
2021-09-21 10:36:36 +0200

    Create the login logic with a basic loading/error state.

--- src/components/oraganisms/AuthFrorm/AuthForm.js
+++ src/components/oraganisms/AuthFrorm/AuthForm.js
@@ -1,17 +1,42 @@
 import Title from 'components/atoms/Title/Title';
 import FormField from 'components/molecules/FormField/FormField';
-import React from 'react';
+import React, { useState } from 'react';
+import { useFirebaseConnect, useFirebase } from 'react-redux-firebase';
 import { StyledButton, StyledForm } from './AuthForm.style';
+import { useForm } from 'react-hook-form';
+
 const AuthForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const { register, handleSubmit } = useForm();
+  useFirebaseConnect();
+  const firebase = useFirebase();
+  const onSubmit = (credentials) => {
+    setIsLoading(true);
+    return firebase
+      .login(credentials)
+      .then(() => setIsLoading(false))
+      .catch((error) => {
+        setIsLoading(false);
+        setIsError(true);
+      });
+  };
+
   return (
-    <StyledForm>
-      <Title titleType="h4">Login</Title>
-      <FormField name="email">Email:</FormField>
-      <FormField name="password" type="password">
-        Password:
-      </FormField>
-      <StyledButton role="submit">Submit</StyledButton>
-    </StyledForm>
+    <>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <Title titleType="h4">Login</Title>
+        <FormField register={register} name="email">
+          Email:
+        </FormField>
+        <FormField register={register} name="password" type="password">
+          Password:
+        </FormField>
+        <StyledButton role="submit">{isLoading ? 'Loading' : 'Submit'}</StyledButton>
+        {isError && !isLoading && <p>it will be an error popup here</p>}
+      </StyledForm>
+      )
+    </>
   );
 };
 
